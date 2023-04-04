b9162b00c59da9343e812ba9ff032561c556fb20
nccharles250 <charles.ndayisaba@amalitech.com>
2023-04-04 20:30:05 +0200

    remove run.sh

--- frontend/source/run.sh
+++
@@ -1,5 +0,0 @@
-# changing the verions    
-sed -i -e "s|\###date###|$(date -u +%F' %T.%3N UTC')|g" app.html
-sed -i -e "s|\###GITHUB_REF_NAME###|$GITHUB_REF_NAME|g" app.html
-sed -i -e "s|\###GITHUB_RUN_NUMBER###|$GITHUB_RUN_NUMBER|g" app.html
-sed -i -e "s|\###GITHUB_SHA###|$GITHUB_SHA|g" app.html
