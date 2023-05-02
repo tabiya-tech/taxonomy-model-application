41a371ce4df98c5f3aa637b0eb0977b4ae1d15fc
Benisis, Apostolos <apostolos.benisis@Apostoloss-MacBook-Pro.local>
2023-05-02 15:55:13 +0300

    chore(ci): make smoke tests reusable

--- backend/test/smoke/smoke.test.ts
+++ backend/test/smoke/smoke.test.ts
@@ -1,17 +1,16 @@
 import {SuperAgentTest} from 'supertest';
 import * as supertest from "supertest";
-//import version from "info/version.json";
 import {StatusCodes} from "httpUtils";
 
 describe("Testing the deployment of the api", () => {
-  const baseUrl: string = process.env.E2E_BASE_URL as string; //"https://dev.tabiya.tech/api"; // TODO: get this from pulumi outputq
+  const baseUrl: string = process.env.E2E_BASE_URL as string;
   beforeAll(() => {
     request = supertest.agent(baseUrl);
   });
 
   let request: SuperAgentTest;
   test("GET /info should respond with the correct version", async () => {
-    const expectedVersion = JSON.parse(process.env.API_VERSION_JSON as string);
+    const expectedVersion = JSON.parse(process.env.EXPECTED_VERSION_INFO as string);
     expect(expectedVersion).toBeDefined();
     const response = await request.get('/info').timeout(5000);
     expect(response.statusCode).toBe(StatusCodes.OK);
--- frontend/test/smoke/smoke.test.ts
+++ frontend/test/smoke/smoke.test.ts
@@ -11,7 +11,7 @@ describe("Testing the deployment of the frontend", () => {
 
   let request: SuperAgentTest;
   test("GET /data/version should respond with the correct version", async () => {
-    const expectedVersion = JSON.parse(process.env.FRONTEND_VERSION_JSON as string);
+    const expectedVersion = JSON.parse(process.env.EXPECTED_VERSION_INFO as string);
     expect(expectedVersion).toBeDefined();
     const response = await request.get('/data/version.json').timeout(5000);
     expect(response.statusCode).toBe(StatusCodes.OK);
