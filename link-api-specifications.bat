55f2af3deb845ce3a8ba5e3bf2c9e99d5384fe99
Benisis, Apostolos <apostolos.benisis@Apostoloss-MacBook-Pro.local>
2023-05-11 13:14:41 +0300

    fix: unlinking api-specifications fails

--- link-api-specifications.bat
+++ link-api-specifications.bat
@@ -1,11 +1,45 @@
 echo "LINKING API-SPECIFICATIONS FOR WINDOWS"
 
-cd api-specifications/
+:: ********************************************************
+:: First unlink to ensure that any previous links is removed
+:: ********************************************************
+pushd backend/
+yarn unlink api-specifications
+:: back to the root of the project
+popd
+pushd frontend/
+yarn unlink api-specifications)
+:: back to the root of the project
+popd
+pushd api-specifications/
+yarn unlink
+cd dist/
+yarn unlink
+:: back to the root of the project
+popd
+
+:: ********************************************************
+:: Install dependencies for the 'api-specifications' package,
+:: compiles it, and creates a symbolic link to the dist/ folder.
+:: ********************************************************
+
+pushd api-specifications/
 yarn install
 yarn run compile
 cd dist/
 yarn link
-cd ../../backend
+:: back to the root of the project
+popd
+
+:: ********************************************************
+:: Finally, link the 'api-specifications' package
+:: to the 'backend' and 'frontend' projects.
+:: ********************************************************
+pushd backend/
 yarn link api-specifications
-cd ../frontend
+:: back to the root of the project
+popd
+pushd frontend/
 yarn link api-specifications
+:: back to the root of the project
+popd
