22197bacbc4893a0a1a50edfd85d488d87e828b8
Benisis, Apostolos <apostolos@benisis.de>
2023-06-17 00:21:56 +0200

    feat(backend): batch process skills to improve import performance [pulumi up]

--- backend/src/import/skills/skillsParser.test.ts
+++ backend/src/import/skills/skillsParser.test.ts
@@ -6,11 +6,18 @@ import {getRepositoryRegistry} from "server/repositoryRegistry/repositoryRegisrt
 import fs from "fs";
 import https from "https";
 import {StatusCodes} from "server/httpUtils";
-import {getRowProcessor, ISkillRow, parseSkillsFromFile, parseSkillsFromUrl} from "./skillsParser";
+import {
+  getRowProcessor,
+  getCompletedProcessor,
+  ISkillRow,
+  parseSkillsFromFile,
+  parseSkillsFromUrl
+} from "./skillsParser";
 import {ISkillRepository} from "esco/skill/SkillRepository";
 import {INewSkillSpec} from "esco/skill/skillModel";
 import {getHeadersValidator} from "./skillsParser";
 import {getStdHeadersValidator} from "import/stdHeadersValidator";
+import {BatchProcessor} from "../batch/BatchProcessor";
 
 jest.mock('https');
 
@@ -18,54 +25,10 @@ jest.mock("import/stdHeadersValidator.ts", () => ({
   getStdHeadersValidator: jest.fn().mockReturnValue(() => true)
 }));
 
-describe("test getRowProcessor", () => {
-  test("should create a skill", async () => {
-    // GIVEN a model id
-    const givenModelId = "modelId";
-    // AND a given row
-    const givenRow: ISkillRow = {
-      ESCOURI: "ESCO:123",
-      ORIGINUUID: "originUUID",
-      PREFERREDLABEL: "preferredLabel",
-      ALTLABELS: "altLabel1\naltLabel2",
-      DESCRIPTION: "description",
-      DEFINITION: "definition",
-      SCOPENOTE: "scopeNote",
-      REUSELEVEL: "cross-sector",
-      SKILLTYPE: "knowledge"
-    };
-    // AND a row processor for the given model id
-    const mockRepository: ISkillRepository = {
-      // @ts-ignore
-      Model: undefined,
-      create: jest.fn().mockResolvedValue({}),
-    };
-    // @ts-ignore
-    jest.spyOn(getRepositoryRegistry(), "skill", "get").mockReturnValue(mockRepository);
-    const rowProcessor = getRowProcessor(givenModelId);
-
-    // WHEN the row processor is called with the given row
-    await rowProcessor(givenRow, 1);
-
-    // THEN expect the skill repository to have been called with the correct spec
-    // @ts-ignore
-    const expectedSpec: INewSkillSpec = {
-      modelId: givenModelId,
-      ESCOUri: "ESCO:123",
-      originUUID: "originUUID",
-      preferredLabel: "preferredLabel",
-      altLabels: ["altLabel1", "altLabel2"],
-      description: "description",
-      definition: "definition",
-      scopeNote: "scopeNote",
-      reuseLevel: "cross-sector",
-      skillType: "knowledge"
-    }
-    expect(mockRepository.create).toBeCalledWith({...expectedSpec, modelId: givenModelId})
-  })
+describe("test headers validator", () => {
   test("should return true if all expected Skill headers are present ", () => {
     // GIVEN a model id
-    const givenModelId = "modelId";
+    const givenModelId = "foo-model-id";
 
     // WHEN the getHeaderValidator is called with the given model id
     getHeadersValidator(givenModelId);
@@ -87,44 +50,80 @@ describe("test getRowProcessor", () => {
     // @ts-ignore
     expect(getStdHeadersValidator).toBeCalledWith(givenModelId, expectedHeaders);
   })
+})
 
-  test("should not fail if creating the skill fails", async () => {
+describe("test getRowProcessor", () => {
+  test("should create a skill", async () => {
     // GIVEN a model id
-    const givenModelId = "modelId";
-    // AND any given row
-    const givenRow: ISkillRow  = {} as ISkillRow;
-    // AND a repository that throws an error
-    const mockRepository: ISkillRepository = {
-      // @ts-ignore
-      Model: undefined,
-      create: jest.fn().mockRejectedValue(new Error("Some Error")),
+    const givenModelId = "foo-model-id";
+    // AND a given row
+    const givenRow: ISkillRow = {
+      ESCOURI: "ESCO:123",
+      ORIGINUUID: "originUUID",
+      PREFERREDLABEL: "preferredLabel",
+      ALTLABELS: "altLabel1\naltLabel2",
+      DESCRIPTION: "description",
+      DEFINITION: "definition",
+      SCOPENOTE: "scopeNote",
+      REUSELEVEL: "cross-sector",
+      SKILLTYPE: "knowledge"
     };
-    // @ts-ignore
-    jest.spyOn(getRepositoryRegistry(), "skill", "get").mockReturnValue(mockRepository);
+    // AND a batch processor
+    const mockProcessBatchFn = jest.fn().mockResolvedValue(undefined);
+    const batchProcessor = new BatchProcessor<INewSkillSpec>(5000, mockProcessBatchFn);
+    jest.spyOn(batchProcessor, "add");
     // AND a row processor for the given model id
-    const rowProcessor = getRowProcessor(givenModelId);
+    const rowProcessor = getRowProcessor(givenModelId, batchProcessor);
 
     // WHEN the row processor is called with the given row
-    const processPromise =  rowProcessor(givenRow, 1);
+    await rowProcessor(givenRow, 1);
 
-    // THEN expect the promise to not be rejected
-    await expect(processPromise).resolves.not.toThrow();
+    // THEN expect specification based on the row to be added to the batch
+    const expectedSpec: INewSkillSpec = {
+      modelId: givenModelId,
+      ESCOUri: "ESCO:123",
+      originUUID: "originUUID",
+      preferredLabel: "preferredLabel",
+      altLabels: ["altLabel1", "altLabel2"],
+      description: "description",
+      definition: "definition",
+      scopeNote: "scopeNote",
+      reuseLevel: "cross-sector",
+      skillType: "knowledge"
+    }
+    expect(batchProcessor.add).toBeCalledWith({...expectedSpec, modelId: givenModelId})
+  })
 
-    // AND expect the skill repository to have been called
-    expect(mockRepository.create).toBeCalled()
-  });
 })
+
+describe("test completed processor", () => {
+  test("should flush the batch processor", async () => {
+    // GIVEN a batch processor with items in the batch queue
+    const mockFlushFn = jest.fn().mockResolvedValue(undefined);
+    const batchProcessor = new BatchProcessor<INewSkillSpec>(5000, mockFlushFn);
+    await batchProcessor.add({} as INewSkillSpec);
+    // AND a completed processor
+    const completedProcessor = getCompletedProcessor(batchProcessor);
+
+    // WHEN the completed processor is called
+    await completedProcessor();
+
+    // THEN expect the batch processor to have been flushed
+    expect(mockFlushFn).toBeCalled();
+  });
+});
 
 describe("test parseSkillsFromUrl", () => {
   test("should create skill from url file", async () => {
     // GIVEN a model id
-    const givenModelId = "modelId";
+    const givenModelId = "foo-model-id";
 
     // AND an ISCOGroup repository
     const mockRepository: ISkillRepository = {
       // @ts-ignore
       Model: undefined,
       create: jest.fn().mockResolvedValue({}),
+      batchCreate: jest.fn().mockResolvedValue([{}])
     };
     // @ts-ignore
     jest.spyOn(getRepositoryRegistry(), "skill", "get").mockReturnValue(mockRepository);
@@ -143,21 +142,25 @@ describe("test parseSkillsFromUrl", () => {
     await parseSkillsFromUrl(givenModelId, "someUrl");
 
     // THEN expect the skill repository to have been called with the correct spec
-    require("./_test_data_/expected.ts").expected.forEach((expectedSpec: Omit<INewSkillSpec, "modelId">, index: number) => {
-      expect(mockRepository.create).toHaveBeenNthCalledWith(index + 1, {...expectedSpec, modelId: givenModelId});
+    require("./_test_data_/expected.ts").expected.forEach((expectedSpec: Omit<INewSkillSpec, "modelId">) => {
+      expect(mockRepository.batchCreate).toHaveBeenLastCalledWith(
+        expect.arrayContaining([{...expectedSpec, modelId: givenModelId}])
+      )
     })
   })
 });
+
 describe("test parseSkillsFromFile", () => {
   test("should create skill from csv file", async () => {
     // GIVEN a model id
-    const givenModelId = "modelId";
+    const givenModelId = "foo-model-id";
 
     // AND a skill repository
     const mockRepository: ISkillRepository = {
       // @ts-ignore
       Model: undefined,
       create: jest.fn().mockResolvedValue({}),
+      batchCreate: jest.fn().mockResolvedValue([{}])
     };
     // @ts-ignore
     jest.spyOn(getRepositoryRegistry(), "skill", "get").mockReturnValue(mockRepository);
@@ -166,8 +169,10 @@ describe("test parseSkillsFromFile", () => {
     await parseSkillsFromFile(givenModelId, "./src/import/skills/_test_data_/given.csv");
 
     // THEN expect the skill repository to have been called with the correct spec
-    require("./_test_data_/expected.ts").expected.forEach((expectedSpec: Omit<INewSkillSpec, "modelId">, index: number) => {
-      expect(mockRepository.create).toHaveBeenNthCalledWith(index + 1, {...expectedSpec, modelId: givenModelId});
+    require("./_test_data_/expected.ts").expected.forEach((expectedSpec: Omit<INewSkillSpec, "modelId">) => {
+      expect(mockRepository.batchCreate).toHaveBeenLastCalledWith(
+        expect.arrayContaining([{...expectedSpec, modelId: givenModelId}])
+      )
     })
   })
 });
