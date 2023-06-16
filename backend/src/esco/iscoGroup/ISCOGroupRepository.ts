6927a7038059edb7aa7a981024702a216f6ced26
Benisis, Apostolos <apostolos@benisis.de>
2023-06-16 20:01:41 +0200

    feat(backend): batchCreate ISCOGroups

--- backend/src/esco/iscoGroup/ISCOGroupRepository.ts
+++ backend/src/esco/iscoGroup/ISCOGroupRepository.ts
@@ -6,10 +6,17 @@ export interface IISCOGroupRepository {
   readonly Model: mongoose.Model<IISCOGroup>;
 
   /**
-   * Resolves to the newly created ISCOGroup entry, or it rejects with an error if the  ISCOGroup entry could not be created.
+   * Resolves to the newly created ISCOGroup entry, or it rejects with an error if the ISCOGroup entry could not be created.
    * @param newISCOGroupSpec
    */
   create(newISCOGroupSpec: INewISCOGroupSpec): Promise<IISCOGroup>;
+
+  /**
+   * Resolves to an array with the newly created ISCOGroup entries. If some of the documents could not be validated, they will be excluded and not saved and the function will resolve.
+   * The promise will reject with an error if the ISCOGroup entries could not be created due to reasons other than not passing the validation.
+   * @param newISCOGroupSpecs
+   */
+  batchCreate(newISCOGroupSpecs: INewISCOGroupSpec[]): Promise<IISCOGroup[]>
 }
 
 export class ISCOGroupRepository implements IISCOGroupRepository {
@@ -36,10 +43,30 @@ export class ISCOGroupRepository implements IISCOGroupRepository {
       });
       await newISCOGroupModel.save();
       await newISCOGroupModel.populate({path: "parentGroup"});
-      await newISCOGroupModel.populate({path:"childrenGroups"});
+      await newISCOGroupModel.populate({path: "childrenGroups"});
       return newISCOGroupModel.toObject();
     } catch (e: unknown) {
       console.error("create failed", e);
+      throw e;
+    }
+  }
+
+  async batchCreate(newISCOGroupSpecs: INewISCOGroupSpec[]): Promise<IISCOGroup[]> {
+    try {
+      const newISCOGroupModels = newISCOGroupSpecs.map((spec) => {
+        return new this.Model({
+          ...spec,
+          parentGroup: null,
+          UUID: randomUUID() // override UUID silently
+        });
+      });
+      const newISCOGroups = await this.Model.insertMany(newISCOGroupModels, {
+        ordered: false,
+        populate: ["parentGroup", "childrenGroups"]
+      });
+      return newISCOGroups.map((iscoGroup) => iscoGroup.toObject());
+    } catch (e: unknown) {
+      console.error("batch create failed", e);
       throw e;
     }
   }
