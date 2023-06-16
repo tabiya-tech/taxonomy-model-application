c7abb6f578bbb00f75d2efdd2082d3a457195f21
Benisis, Apostolos <apostolos@benisis.de>
2023-06-17 00:21:56 +0200

    feat(backend): batch process skills to improve import performance [pulumi up]

--- backend/src/import/skills/skillsParser.ts
+++ backend/src/import/skills/skillsParser.ts
@@ -1,8 +1,15 @@
 import {getRepositoryRegistry} from "server/repositoryRegistry/repositoryRegisrty";
-import {HeadersValidatorFunction, processDownloadStream, processStream} from "import/stream/processStream";
+import {
+  CompletedFunction,
+  HeadersValidatorFunction,
+  processDownloadStream,
+  processStream,
+  RowProcessorFunction
+} from "import/stream/processStream";
 import fs from "fs";
 import {INewSkillSpec, ReuseLevel, SkillType} from "esco/skill/skillModel";
-import {getStdHeadersValidator} from "../stdHeadersValidator";
+import {getStdHeadersValidator} from "import/stdHeadersValidator";
+import {BatchProcessor} from "import/batch/BatchProcessor";
 
 // expect all columns to be in upper case
 export interface ISkillRow {
@@ -21,10 +28,21 @@ export function getHeadersValidator(modelId: string): HeadersValidatorFunction {
   return getStdHeadersValidator(modelId, ['ESCOURI', 'ORIGINUUID', 'PREFERREDLABEL', 'ALTLABELS', 'DESCRIPTION', 'DEFINITION', 'SCOPENOTE', 'REUSELEVEL', 'SKILLTYPE']);
 }
 
-export function getRowProcessor(modelId: string): (row: ISkillRow, index: number) => Promise<void> {
+function getBatchProcessor() {
+  const BATCH_SIZE: number = 5000;
+  const batchProcessFn = async (specs: INewSkillSpec[]) => {
+    try {
+      const skillRepository = getRepositoryRegistry().skill;
+      await skillRepository.batchCreate(specs);
+    } catch (e: unknown) {
+      console.error("Failed to process batch", e);
+    }
+  };
+  return new BatchProcessor<INewSkillSpec>(BATCH_SIZE, batchProcessFn);
+}
 
-  const skillRepository = getRepositoryRegistry().skill;
-  return async (row: ISkillRow, index: number) => {
+export function getRowProcessor(modelId: string, batchProcessor: BatchProcessor<INewSkillSpec>): RowProcessorFunction<ISkillRow> {
+  return async (row: ISkillRow) => {
     // @ts-ignore
     const spec: INewSkillSpec = {
       ESCOUri: row.ESCOURI ?? '',
@@ -38,25 +56,30 @@ export function getRowProcessor(modelId: string): (row: ISkillRow, index: number
       reuseLevel: row.REUSELEVEL as ReuseLevel ?? '',
       skillType: row.SKILLTYPE as SkillType ?? '',
     };
-    try {
-      await skillRepository.create(spec);
-      //eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (e: any) {
-      console.warn(`Failed to process row(${index}):'${row}' create skill ${JSON.stringify(spec)}: ${e.message}`);
-    }
+    await batchProcessor.add(spec);
+  };
+}
+
+export function getCompletedProcessor(batchProcessor: BatchProcessor<INewSkillSpec>): CompletedFunction {
+  return async () => {
+    await batchProcessor.flush();
   };
 }
 
 // function to parse from url
 export async function parseSkillsFromUrl(modelId: string, url: string) {
   const headersValidator = getHeadersValidator(modelId);
-  const rowProcessor = getRowProcessor(modelId);
-  await processDownloadStream(url, headersValidator, rowProcessor);
+  const batchProcessor = getBatchProcessor();
+  const rowProcessor = getRowProcessor(modelId, batchProcessor);
+  const completedProcessor = getCompletedProcessor(batchProcessor);
+  await processDownloadStream(url, headersValidator, rowProcessor, completedProcessor);
 }
 
 export async function parseSkillsFromFile(modelId: string, filePath: string) {
-  const headersValidator = getHeadersValidator(modelId);
   const skillsCSVFileStream = fs.createReadStream(filePath);
-  const rowProcessor = getRowProcessor(modelId);
-  await processStream<ISkillRow>(skillsCSVFileStream, headersValidator, rowProcessor);
+  const headersValidator = getHeadersValidator(modelId);
+  const batchProcessor = getBatchProcessor();
+  const rowProcessor = getRowProcessor(modelId, batchProcessor);
+  const completedProcessor = getCompletedProcessor(batchProcessor);
+  await processStream<ISkillRow>(skillsCSVFileStream, headersValidator, rowProcessor, completedProcessor);
 }
