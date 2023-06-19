2508594642cc26704ff7e90ab07c18fb1e20ce8c
Benisis, Apostolos <apostolos@benisis.de>
2023-06-19 19:55:28 +0200

    chore(backend): replace 'fori' with 'forof'

--- backend/src/import/parse/stdHeadersValidator.ts
+++ backend/src/import/parse/stdHeadersValidator.ts
@@ -1,10 +1,10 @@
 export function getStdHeadersValidator(modelid: string,  expectedHeaders: string []): (actualHeaders: string[]) => Promise<boolean> {
   return async (actualHeaders: string[]) => {
     let valid = true;
-    for (let i = 0; i < expectedHeaders.length; i++) {
-      if (!actualHeaders.includes(expectedHeaders[i])) {
+    for (const element of expectedHeaders) {
+      if (!actualHeaders.includes(element)) {
         valid = false;
-        console.warn(`When importing data for model ${modelid}, expected to include header ${expectedHeaders[i]}`);
+        console.warn(`When importing data for model ${modelid}, expected to include header ${element}`);
       }
     }
     return valid;
