f7d19352583c66310208f5c8d2a3764e830f5f99
Benisis, Apostolos <apostolos@benisis.de>
2023-07-25 15:45:15 +0300

    chore: improve test readability for backend/modelInfo/index.test.ts

--- backend/src/modelInfo/index.test.ts
+++ backend/src/modelInfo/index.test.ts
@@ -18,12 +18,12 @@ import {getRepositoryRegistry} from "server/repositoryRegistry/repositoryRegisrt
 const transformSpy = jest.spyOn(transformModule, "transform");
 
 
-describe("test for model handler", () => {
+describe("Test for model handler", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it("POST should respond with the CREATED and the model, ", async () => {
+  test("POST should respond with the CREATED status code and the newly created modelInfo", async () => {
     // GIVEN a valid request (method & header & payload)
     const givenPayload: IModelInfoRequest = {
       name: getRandomString(NAME_MAX_LENGTH),
@@ -44,37 +44,34 @@ describe("test for model handler", () => {
     // AND a configured base path for resources
     const givenResourcesBaseUrl = "https://some/path/to/api/resources";
     jest.spyOn(config, "getResourcesBaseUrl").mockReturnValueOnce(givenResourcesBaseUrl)
-
-    // AND repository successfully creates a model
-    const expectedModelInfo: IModelInfo = getIModelInfoMockData();
-
-    const modelInfoMock = {
-      Model: undefined,
-      create: jest.fn().mockResolvedValue(expectedModelInfo),
+    // AND a repository that will successfully create a model
+    const givenModelInfo: IModelInfo = getIModelInfoMockData();
+    const givenModelInfoRepositoryMock = {
+      Model: undefined as any,
+      create: jest.fn().mockResolvedValue(givenModelInfo),
       getModelById: jest.fn().mockResolvedValue(null),
       getModelByUUID: jest.fn().mockResolvedValue(null)
     };
-    // @ts-ignore
-    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(modelInfoMock);
+    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(givenModelInfoRepositoryMock);
 
-    //WHEN the info handler is invoked with the event
-    //@ts-ignore
+    // WHEN the info handler is invoked with the given event
+    // @ts-ignore
     const actualResponse = await modelHandler(givenEvent);
 
-    // THEN expect the handler to call the repository with the payload
+    // THEN expect the handler to call the repository with the given payload
     expect(getRepositoryRegistry().modelInfo.create).toHaveBeenCalledWith(givenPayload);
-    // AND expect to respond with status CREATED
+    // AND respond with the CREATED status
     expect(actualResponse.statusCode).toEqual(StatusCodes.CREATED);
-    // AND expect the handler to return the correct headers
+    // AND the handler to return the correct headers
     expect(actualResponse.headers).toMatchObject({"Content-Type": "application/json"});
     // AND the transformation function is called correctly
-    expect(transformModule.transform).toHaveBeenCalledWith(expectedModelInfo, givenResourcesBaseUrl);
-    // AND expect the handler to return the result of the transformation function
+    expect(transformModule.transform).toHaveBeenCalledWith(givenModelInfo, givenResourcesBaseUrl);
+    // AND the handler to return the result of the transformation function
     expect(JSON.parse(actualResponse.body)).toMatchObject(transformSpy.mock.results[0].value);
   });
 
-  it('POST should respond with the INTERNAL_SERVER_ERROR if it fails to repository failed to respond ', async () => {
-    //GIVEN a valid payload
+  test('POST should respond with the INTERNAL_SERVER_ERROR status code if the repository fails to create the model info', async () => {
+    // GIVEN a valid request (method & header & payload)
     const givenPayload = {
       name: 'foo',
       locale: {
@@ -84,8 +81,6 @@ describe("test for model handler", () => {
       },
       description: 'some text',
     }
-
-    // AND event
     const givenEvent = {
       httpMethod: HTTP_VERBS.POST,
       body: JSON.stringify(givenPayload),
@@ -93,26 +88,25 @@ describe("test for model handler", () => {
         'Content-Type': 'application/json'
       }
     }
-
-    // AND repository fails to  create a model
-    const modelInfoMock = {
+    // AND the repository fails to create a model
+    const givenModelInfoRepositoryMock = {
       Model: undefined,
       create: jest.fn().mockRejectedValue(new Error("foo")),
       getModelById: jest.fn().mockResolvedValue(null),
       getModelByUUID: jest.fn().mockResolvedValue(null)
     };
     // @ts-ignore
-    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(modelInfoMock);
+    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(givenModelInfoRepositoryMock);
 
-    //WHEN the info handler is invoked with event param
-    //@ts-ignore
-    const actualResponse = await modelHandler(givenEvent, null, null);
-    // THEN expects created is called
+    // WHEN the info handler is invoked with the event param
+    // @ts-ignore
+    const actualResponse = await modelHandler(givenEvent);
+
+    // THEN expect the handler to call the repository with the given payload
     expect(getRepositoryRegistry().modelInfo.create).toHaveBeenCalledWith(givenPayload);
-    //AND  expect response to be Created and the model
+    // AND to respond with the INTERNAL_SERVER_ERROR status
     expect(actualResponse.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
-    // AND expect  created
-    // error response status >=400 represents an error
+    // AND the response body to be the expectedErrorBody
     const expectedErrorBody: IErrorResponse = {
       "errorCode": ModelInfoResponseErrorCodes.DB_FAILED_TO_CREATE_MODEL,
       "message": "Failed to create the model in the DB",
@@ -121,28 +115,28 @@ describe("test for model handler", () => {
     expect(JSON.parse(actualResponse.body)).toMatchObject(expectedErrorBody);
   });
 
-  it.each([
+  test.each([
     HTTP_VERBS.PUT,
     HTTP_VERBS.DELETE,
     HTTP_VERBS.OPTIONS,
     HTTP_VERBS.PATCH,
     HTTP_VERBS.GET
   ])
-  ("%s should respond with NOT_FOUND error",
+  ("%s should respond with METHOD_NOT_ALLOWED error",
     async (param) => {
-      //GIVEN an event with a non POST method
-      const event = {httpMethod: param};
+      // GIVEN an event with a non POST method
+      const givenEvent = {httpMethod: param};
 
-      //WHEN the model handler is invoked
-      //@ts-ignore
-      const actualResponse = await modelHandler(event, null, null);
+      // WHEN the model handler is invoked with the given event
+      // @ts-ignore
+      const actualResponse = await modelHandler(givenEvent);
 
-      //THEN expect status to be 400
+      // THEN expect the handler to respond with METHOD_NOT_ALLOWED status
       expect(actualResponse).toEqual(STD_ERRORS_RESPONSES.METHOD_NOT_ALLOWED);
     });
 
-  it("POST should respond with the UNSUPPORTED_MEDIA_TYPE if content type is invalid, ", async () => {
-    //GIVEN a payload
+  test("POST should respond with the UNSUPPORTED_MEDIA_TYPE status code if the content type is not application/json", async () => {
+    // GIVEN a request (method & header & payload) with a non application/json content type
     const givenPayload = {
       previousUUID: randomUUID(),
       originUUID: randomUUID(),
@@ -156,8 +150,6 @@ describe("test for model handler", () => {
       releaseNotes: 'some text',
       version: 'v1.0.0'
     }
-
-    // AND event
     const givenEvent = {
       httpMethod: HTTP_VERBS.POST,
       body: JSON.stringify(givenPayload),
@@ -165,28 +157,28 @@ describe("test for model handler", () => {
         'Content-Type': 'text/html'
       }
     }
-
-    const modelInfoMock = {
+    // AND the repository would return some model info
+    const givenModelInfoRepositoryMock = {
       Model: undefined,
       create: jest.fn().mockResolvedValue({}),
       getModelById: jest.fn().mockResolvedValue(null),
       getModelByUUID: jest.fn().mockResolvedValue(null)
     };
-    // @ts-ignore
-    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(modelInfoMock);
 
-    //WHEN the info handler is invoked with event param
-    //@ts-ignore
-    const actualResponse = await modelHandler(givenEvent, null, null);
-    //THEN  expect response to be Created and the model
+    // @ts-ignore
+    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(givenModelInfoRepositoryMock);
+
+    // WHEN the info handler is invoked with the given event request
+    // @ts-ignore
+    const actualResponse = await modelHandler(givenEvent);
+
+    // THEN expect the handler to respond with UNSUPPORTED_MEDIA_TYPE status
     expect(actualResponse.statusCode).toEqual(StatusCodes.UNSUPPORTED_MEDIA_TYPE);
-    // AND expect  created
   });
 
-  test("POST should respond with the BAD_REQUEST if payload doe not conform to the model schema", async () => {
-    //GIVEN a payload that does not conform to the model schema
+  test("POST should respond with the BAD_REQUEST status code if payload does not conform to the model json schema", async () => {
+    // GIVEN a request (method & header & a payload that does not conform to the model info schema)
     const givenPayload = {foo: "foo"}
-    // AND event
     const givenEvent = {
       httpMethod: HTTP_VERBS.POST,
       body: JSON.stringify(givenPayload),
@@ -194,30 +186,27 @@ describe("test for model handler", () => {
         'Content-Type': 'application/json'
       }
     }
-    // AND error response status =400 represents an error
+    // AND the repository would return some model info
+    const givenModelInfoRepositoryMock = {
+      Model: undefined as any,
+      create: jest.fn().mockResolvedValue({}),
+      getModelById: jest.fn().mockResolvedValue(null),
+      getModelByUUID: jest.fn().mockResolvedValue(null)
+    };
+    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(givenModelInfoRepositoryMock);
+
+    // WHEN the info handler is invoked with the given event
+    // @ts-ignore
+    const actualResponse = await modelHandler(givenEvent);
+
+    // THEN expect the handler to respond with BAD_REQUEST status
+    expect(actualResponse.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    // AND the response body to be the expectedErrorBody
     const expectedErrorBody: IErrorResponse = {
       "errorCode": ModelInfoResponseErrorCodes.MODEL_COULD_NOT_VALIDATE,
       "message": "Payload should conform to schema",
       "details": "Payload should conform to schema",
     }
-
-
-    const modelInfoMock = {
-      Model: undefined,
-      create: jest.fn().mockResolvedValue({}),
-      getModelById: jest.fn().mockResolvedValue(null),
-      getModelByUUID: jest.fn().mockResolvedValue(null)
-    };
-    // @ts-ignore
-    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(modelInfoMock);
-
-
-    //WHEN the info handler is invoked with event param
-    //@ts-ignore
-    const actualResponse = await modelHandler(givenEvent, null, null);
-    //THEN  expect response to be Created and the model
-    expect(actualResponse.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-    // AND expect  created
     expect(JSON.parse(actualResponse.body)).toEqual(
       expect.objectContaining({
         errorCode: expectedErrorBody.errorCode,
@@ -227,15 +216,13 @@ describe("test for model handler", () => {
 
   });
 
-
   test.each([
-    ["is a malformed json", '{'],
-    ["is a string", 'foo'],
+    ["a malformed json", '{'],
+    ["a string", 'foo'],
   ])
   ("POST should respond with the BAD_REQUEST if model body is %s", async (description, payload) => {
-    //GIVEN a payload
+    // GIVEN a request (method & header & payload)
     const givenPayload = payload
-    // AND event
     const givenEvent = {
       httpMethod: HTTP_VERBS.POST,
       body: givenPayload,
@@ -243,28 +230,28 @@ describe("test for model handler", () => {
         'Content-Type': 'application/json'
       }
     }
-    // AND error response status =400 represents an error
-    const expectedErrorBody: IErrorResponse = {
-      "errorCode": ErrorCodes.MALFORMED_BODY,
-      "message": "Payload is malformed, it should be a valid model json",
-      "details": "any text",
-    }
-
-    const modelInfoMock = {
+    // AND the repository would return some model info
+    const givenModelInfoRepositoryMock = {
       Model: undefined,
       create: jest.fn().mockResolvedValue({}),
       getModelById: jest.fn().mockResolvedValue(null),
       getModelByUUID: jest.fn().mockResolvedValue(null)
     };
     // @ts-ignore
-    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(modelInfoMock);
+    jest.spyOn(getRepositoryRegistry(), "modelInfo", "get").mockReturnValue(givenModelInfoRepositoryMock);
 
-    //WHEN the info handler is invoked with event param
-    //@ts-ignore
+    // WHEN the info handler is invoked with then given event
+    // @ts-ignore
     const actualResponse = await modelHandler(givenEvent, null, null);
-    //THEN  expect response to be Created and the model
+
+    // THEN expect the handler to respond with BAD_REQUEST status
     expect(actualResponse.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-    // AND expect  created
+    // AND the response body to be the expectedErrorBody
+    const expectedErrorBody: IErrorResponse = {
+      "errorCode": ErrorCodes.MALFORMED_BODY,
+      "message": "Payload is malformed, it should be a valid model json",
+      "details": "any text",
+    }
     expect(JSON.parse(actualResponse.body)).toEqual(
       expect.objectContaining({
         errorCode: expectedErrorBody.errorCode,
