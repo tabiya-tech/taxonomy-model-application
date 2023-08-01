1c5a1ff1999a5d8bdca62cad7fb0650530f58824
Merci Jacob <mercihabam@gmail.com>
2023-08-01 15:41:14 +0200

    chore: Improve test readability for backend/esco/skill/SkillRepository.test.ts

--- backend/src/esco/skill/SkillRepository.test.ts
+++ backend/src/esco/skill/SkillRepository.test.ts
@@ -48,7 +48,7 @@ function getNewSkillSpec(): INewSkillSpec {
 }
 
 /**
- * Helper function to create an expected INewSkillSpec from a given ,
+ * Helper function to create an expected INewSkillSpec from a given INewSkillSpec,
  * that can ebe used for assertions
  * @param givenSpec
  */
@@ -110,27 +110,30 @@ describe("Test the Skill Repository with an in-memory mongodb", () => {
     })
 
     test("should successfully create a new skill", async () => {
-      // GIVEN a valid SkillSpec
+      // GIVEN a valid newSkillSpec
       const givenNewSkillSpec: INewSkillSpec = getNewSkillSpec();
 
-      // WHEN Creating a new skill with given specifications
-      const newModel = await repository.create(givenNewSkillSpec);
+      // WHEN Creating a new skill with the given specifications
+      const actualNewSkill = await repository.create(givenNewSkillSpec);
 
       // THEN expect the new skill to be created with the specific attributes
       const expectedNewSkill: ISkill = expectedFromGivenSpec(givenNewSkillSpec);
-      expect(newModel).toEqual(expectedNewSkill);
+      expect(actualNewSkill).toEqual(expectedNewSkill);
     });
 
     test("should reject with an error when creating a skill and providing a UUID", async () => {
-      // GIVEN a SkillSpec that is otherwise valid but has a UUID
+      // GIVEN a valid newSkillSpec
       const givenNewSkillSpec: INewSkillSpec = getNewSkillSpec();
 
-      // WHEN Creating a new SkillSpec with the UUID of the existing
-      await expect(repository.create({
+      // WHEN Creating a new skill with the given specifications by providing a UUID
+      const actualPromise = repository.create({
         ...givenNewSkillSpec,
         //@ts-ignore
         UUID: randomUUID()
-      })).rejects.toThrowError(/UUID should not be provided/);
+      });
+      
+      // THEN expect the actual promise to reject
+      await expect(actualPromise).rejects.toThrowError(/UUID should not be provided/);
     });
 
     test("should reject with an error when creating skill with an existing UUID", async () => {
@@ -141,7 +144,10 @@ describe("Test the Skill Repository with an in-memory mongodb", () => {
       // WHEN Creating a new Skill with the UUID of the existing Skill
       // @ts-ignore
       randomUUID.mockReturnValueOnce(givenNewModel.UUID);
-      await expect(repository.create(givenNewSkillSpecSpec)).rejects.toThrowError(/duplicate key/);
+      const actualPromise = repository.create(givenNewSkillSpecSpec)
+      
+      // THEN expect the actual promise to reject
+      await expect(actualPromise).rejects.toThrowError(/duplicate key/);
     });
 
     TestConnectionFailure((repository) => {
@@ -159,18 +165,18 @@ describe("Test the Skill Repository with an in-memory mongodb", () => {
     })
 
     test("should successfully create a batch of new Skills", async () => {
-      // GIVEN some valid SkillSpec
+      // GIVEN some valid SkillSpecs
       const givenBatchSize = 3;
       const givenNewSkillSpecs: INewSkillSpec[] = [];
       for (let i = 0; i < givenBatchSize; i++) {
         givenNewSkillSpecs[i] = getNewSkillSpec();
       }
 
-      // WHEN batch creating the Skills with the given specifications
-      const newSkills: INewSkillSpec[] = await repository.createMany(givenNewSkillSpecs);
+      // WHEN creating the batch of skills with the given specifications
+      const actualNewSkills: INewSkillSpec[] = await repository.createMany(givenNewSkillSpecs);
 
       // THEN expect all the Skills to be created with the specific attributes
-      expect(newSkills).toEqual(
+      expect(actualNewSkills).toEqual(
         expect.arrayContaining(
           givenNewSkillSpecs.map((givenNewSkillSpec) => {
             return expectedFromGivenSpec(givenNewSkillSpec);
@@ -180,26 +186,25 @@ describe("Test the Skill Repository with an in-memory mongodb", () => {
     });
 
     test("should successfully create a batch of new Skills even if some don't validate", async () => {
-      // GIVEN two valid ISCOGroupSpec
+      // GIVEN two valid skillSpecs
       const givenValidSkillSpecs: INewSkillSpec[] = [getNewSkillSpec(), getNewSkillSpec()];
 
-      // AND two SkillSpec that is invalid
+      // AND two invalid others
       const givenInvalidSkillSpec: INewSkillSpec [] = [getNewSkillSpec(), getNewSkillSpec()];
       givenInvalidSkillSpec[0].preferredLabel = ""; // will not validate but will not throw an error
       // @ts-ignore
       givenInvalidSkillSpec[1].foo = "invalid"; // will not validate and will throw an error
 
-      // WHEN batch creating the ISCO Groups with the given specifications
-      const newSkills: INewSkillSpec[] = await repository.createMany([
+      // WHEN creating the batch of skills with the given specifications
+      const actualNewSkills: INewSkillSpec[] = await repository.createMany([
         givenValidSkillSpecs[0],
         ...givenInvalidSkillSpec,
         givenValidSkillSpecs[1],
       ]);
 
       // THEN expect only the valid Skills to be created
-      expect(newSkills).toHaveLength(givenValidSkillSpecs.length);
-
-      expect(newSkills).toEqual(
+      expect(actualNewSkills).toHaveLength(givenValidSkillSpecs.length);
+      expect(actualNewSkills).toEqual(
         expect.arrayContaining(
           givenValidSkillSpecs.map((givenNewSkillSpec) => {
             return expectedFromGivenSpec(givenNewSkillSpec);
@@ -211,36 +216,35 @@ describe("Test the Skill Repository with an in-memory mongodb", () => {
     test("should resolve to an empty array if none of the element could be validated", async () => {
       // GIVEN only invalid SkillSpec
       const givenBatchSize = 3;
-      const givenValidSkillSpecs: INewSkillSpec[] = [];
+      const givenInValidSkillSpecs: INewSkillSpec[] = [];
       for (let i = 0; i < givenBatchSize; i++) {
-        givenValidSkillSpecs[i] = getNewSkillSpec();
-        givenValidSkillSpecs[i].preferredLabel = "";
+        givenInValidSkillSpecs[i] = getNewSkillSpec();
+        givenInValidSkillSpecs[i].preferredLabel = "";
       }
-      // WHEN batch creating the Skill with the given specifications
-      const newSkills: INewSkillSpec[] = await repository.createMany(givenValidSkillSpecs);
+      
+      // WHEN creating the batch of skills with the given specifications
+      const actualNewSkills: INewSkillSpec[] = await repository.createMany(givenInValidSkillSpecs);
 
-      // THEN expect an empty array to be created
-      expect(newSkills).toHaveLength(0);
+      // THEN expect no skill to be created
+      expect(actualNewSkills).toHaveLength(0);
     });
 
     describe("Test unique indexes", () => {
       test("should return only the documents that did not violate the UUID unique index", async () => {
-        // GIVEN 3 Skill
+        // GIVEN 3 SkillSpecs
         const givenBatchSize = 3;
         const givenNewSkillSpecs: INewSkillSpec[] = [];
         for (let i = 0; i < givenBatchSize; i++) {
           givenNewSkillSpecs[i] = getNewSkillSpec();
         }
 
-        // WHEN batch creating the Skill with the given specifications
-        // AND the second SkillSpec is created with the same UUID as the first one
+        // WHEN creating the batch of skills with the given specifications (the second SkillSpec having the same UUID as the first one)
         (randomUUID as jest.Mock).mockReturnValueOnce("014b0bd8-120d-4ca4-b4c6-40953b170219");
         (randomUUID as jest.Mock).mockReturnValueOnce("014b0bd8-120d-4ca4-b4c6-40953b170219");
+        const actualNewSkills: INewSkillSpec[] = await repository.createMany(givenNewSkillSpecs);
 
-        const newSkills: INewSkillSpec[] = await repository.createMany(givenNewSkillSpecs);
-
-        // THEN expect only the first and the third the Skill to be created with the specific attributes
-        expect(newSkills).toEqual(
+        // THEN expect only the first and the third Skill to be created with the specific attributes
+        expect(actualNewSkills).toEqual(
           expect.arrayContaining(
             givenNewSkillSpecs.filter((spec, index) => index !== 1)
               .map((givenNewSkillSpec) => {
@@ -273,7 +277,7 @@ function TestConnectionFailure(actionCallback: (repository: ISkillRepository) =>
     await repositoryRegistry.initialize(connection);
     const repository = repositoryRegistry.skill;
 
-    // WHEN connection is losta
+    // WHEN connection is lost
     await connection.close(false);
 
     // THEN expect to reject with an error
