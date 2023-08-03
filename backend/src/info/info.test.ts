dc99e93948d8245f791650db6ec1a82e7a0b1cce
Fides <fniragena@gmail.com>
2023-08-03 15:51:39 +0200

    chore: use reusable std rest api tests for backend/src/info/info.test.ts

--- backend/src/info/info.test.ts
+++ backend/src/info/info.test.ts
@@ -1,24 +1,25 @@
 import {handler as infoHandler} from "./index";
-import {HTTP_VERBS, StatusCodes, STD_ERRORS_RESPONSES} from "server/httpUtils";
+import {HTTP_VERBS, StatusCodes} from "server/httpUtils";
 import version from './version.json';
 import * as config from "server/config/config";
+import {testMethodsNotAllowed} from "_test_utilities/stdRESTHandlerTests";
 
 describe("test for info handler", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   })
-  it("GET should respond with the 200 and the version, ", async () => {
-    //GIVEN some configuration
+  it("GET should respond with the OK and the version, ", async () => {
+    // GIVEN some configuration
     const givenResourcesBaseUrl = "https://some/path/to/api/resources";
     jest.spyOn(config, "getResourcesBaseUrl").mockReturnValue(givenResourcesBaseUrl)
-    // AND  GET event
+    // AND GET event
     const givenEvent = {
       httpMethod: HTTP_VERBS.GET,
       path: "/info"
     }
 
-    //WHEN the info handler is invoked with event param
-    //@ts-ignore
+    // WHEN the info handler is invoked with event param
+    // @ts-ignore
     const actualResponse = await infoHandler(givenEvent, null, null);
 
     // THEN expect response to be OK and the version
@@ -31,22 +32,11 @@ describe("test for info handler", () => {
     });
   })
 
-  it.each([
+  testMethodsNotAllowed([
     HTTP_VERBS.PUT,
     HTTP_VERBS.DELETE,
     HTTP_VERBS.OPTIONS,
     HTTP_VERBS.PATCH,
     HTTP_VERBS.POST
-  ])("%s should respond with 404 error",
-    async (param) => {
-      //GIVEN an event with a non GET method
-      const event = {httpMethod: param};
-
-      //WHEN the info handler is invoked
-      //@ts-ignore
-      const actualResponse = await infoHandler(event, null, null);
-
-      //THEN expect status to be 400
-      expect(actualResponse).toEqual(STD_ERRORS_RESPONSES.METHOD_NOT_ALLOWED);
-    });
+  ], infoHandler)
 })
