13a05b353eabedff0ab7eaf825036c451c2d5b5c
Fides <fniragena@gmail.com>
2023-08-03 16:36:05 +0200

    chore:improve test readability for backend/src/presigned/index.integration.test.ts

--- backend/src/presigned/index.integration.test.ts
+++ backend/src/presigned/index.integration.test.ts
@@ -1,6 +1,5 @@
 // ####
 // Set up the mock for the S3Client
-
 jest.mock("@aws-sdk/client-s3", () => {
   return {
     S3Client: jest.fn()
@@ -9,7 +8,10 @@ jest.mock("@aws-sdk/client-s3", () => {
 // Set up the mock for the createPresignedPost
 jest.mock("@aws-sdk/s3-presigned-post", () => {
   return {
-    createPresignedPost: jest.fn().mockResolvedValue({url: "https://some/path/to/bucket", fields: {"key": "value", "key1": "value", "key2": "value2"}})
+    createPresignedPost: jest.fn().mockResolvedValue({
+      url: "https://some/path/to/bucket",
+      fields: {"key": "value", "key1": "value", "key2": "value2"}
+    })
   }
 });
 // ####
@@ -23,31 +25,28 @@ import addFormats from "ajv-formats";
 
 describe('test main handler by mocking the aws S3', () => {
   it("GET should respond with the OK and response passes the JSON Schema validation", async () => {
-    // GIVEN a given region from the configuration
+    // GIVEN a region & a bucketName have been configured
     const givenRegion = "foo";
-    //@ts-ignore
-    jest.spyOn(config, "getUploadBucketRegion").mockReturnValue(givenRegion);
-    // AND a given bucket name from the configuration
     const givenBucketName = "bar";
-    //@ts-ignore
+    jest.spyOn(config, "getUploadBucketRegion").mockReturnValue(givenRegion);
     jest.spyOn(config, "getUploadBucketName").mockReturnValue(givenBucketName);
-
     // AND a GET event
-    const mockEvent= {
+    const givenEvent = {
       httpMethod: HTTP_VERBS.GET,
     }
-    // WHEN getPreSigned() is called
-    // @ts-ignore
-    const actualResponse = await handlerModule.handler(mockEvent);
 
-    // THEN expect to respond with status OK
+    // WHEN the main handler is called with the given event
+    // @ts-ignore
+    const actualResponse = await handlerModule.handler(givenEvent);
+
+    // THEN expect the handler to respond with status OK
     expect(actualResponse.statusCode).toEqual(StatusCodes.OK);
-    // AND expect the handler to return the correct headers
+    // AND to return the correct headers
     expect(actualResponse.headers).toMatchObject({"Content-Type": "application/json"});
-    // AND it should the response body should validate against the PresignedResponseSchema schema
-    const ajv = new Ajv({validateSchema: true, strict: true, allErrors: true});
-    addFormats(ajv);
-    const validateResponse = ajv.compile(PresignedResponseSchema);
+    // AND the body should successfully validate against the PresignedResponseSchema schema
+    const expectedAjv = new Ajv({validateSchema: true, strict: true, allErrors: true});
+    addFormats(expectedAjv);
+    const validateResponse = expectedAjv.compile(PresignedResponseSchema);
     validateResponse(JSON.parse(actualResponse.body));
     expect(validateResponse.errors).toBeNull();
   });
