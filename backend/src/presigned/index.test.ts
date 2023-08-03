3572eb607f7d14dfda2554520ff14a493a90cad0
Fides <fniragena@gmail.com>
2023-08-03 19:02:43 +0200

    chore:improve test readability for backend/src/presigned/index.test.ts

--- backend/src/presigned/index.test.ts
+++ backend/src/presigned/index.test.ts
@@ -2,6 +2,14 @@
 // Set up mocks
 // Set up crypto.randomUUID mock
 import {EXPIRES, MAX_FILE_SIZE} from "./presigned.constants";
+import {randomUUID} from "crypto";
+import * as config from "server/config/config";
+import * as handlerModule from './index';
+import {APIGatewayProxyEvent} from 'aws-lambda';
+import {HTTP_VERBS, StatusCodes, STD_ERRORS_RESPONSES} from "server/httpUtils";
+import * as awsSDKServiceModule from "./awsSDKService";
+import * as transformModule from "./transform";
+import {testMethodsNotAllowed} from "../_test_utilities/stdRESTHandlerTests";
 
 jest.mock("crypto", () => {
   const actual = jest.requireActual("crypto");
@@ -11,97 +19,70 @@ jest.mock("crypto", () => {
   }
 });
 
-// ############
-
-import {randomUUID} from "crypto";
-import * as config from "server/config/config";
-import * as handlerModule from './index';
-import {APIGatewayProxyEvent} from 'aws-lambda';
-import {HTTP_VERBS, StatusCodes} from "server/httpUtils";
-import * as awsSDKServiceModule from "./awsSDKService";
-import * as transformModule from "./transform";
-
 beforeEach(() => {
   jest.clearAllMocks();
 });
 
 describe('test main handler', () => {
 
-  it("GET should respond with whatever the getPresigned returns", async () => {
+  it("GET should respond with whatever the getPresigned function returns", async () => {
     // GIVEN a GET event
-    const mockEvent: APIGatewayProxyEvent = {
+    const givenEvent: APIGatewayProxyEvent = {
       httpMethod: HTTP_VERBS.GET,
     } as any;
-
-    // AND getPresigned() returns some response
+    // AND the getPresigned() function returns some response
     const givenResponse = {foo: "foo"} as any;
     jest.spyOn(handlerModule, "getPreSigned").mockReturnValueOnce(givenResponse);
 
-    // WHEN the main handler is called with that event
-    const result = await handlerModule.handler(mockEvent);
+    // WHEN the main handler is called with the given event
+    const actualResponse = await handlerModule.handler(givenEvent);
 
-    // THEN it should return whatever getPreSigned returns
-    expect(result).toEqual(givenResponse)
+    // THEN expect the handler to return the response that the getPresigned function returned
+    expect(actualResponse).toEqual(givenResponse);
   });
-
-  it.each([
-    [HTTP_VERBS.POST],
-    [HTTP_VERBS.PUT],
-    [HTTP_VERBS.DELETE],
-    [HTTP_VERBS.PATCH],
-    [HTTP_VERBS.OPTIONS],
-  ])("should return a 405 response when HTTP method is %s", async (method) => {
-    // GIVEN a GET event
-    const mockEvent: APIGatewayProxyEvent = {
-      httpMethod: method,
-    } as any;
-
-    // WHEN the handler is called with that event
-    const result = await handlerModule.handler(mockEvent);
-
-    // THEN it should return a 405 response
-    expect(result.statusCode).toBe(StatusCodes.METHOD_NOT_ALLOWED);
-  });
+  
+  testMethodsNotAllowed([
+    HTTP_VERBS.POST,
+    HTTP_VERBS.PUT,
+    HTTP_VERBS.DELETE,
+    HTTP_VERBS.PATCH,
+    HTTP_VERBS.OPTIONS
+  ], handlerModule.handler);
 
 });
 
 describe('test getPreSigned()', () => {
-  it("should response with OK and the pre-signed data", async () => {
-    // GIVEN a given region from the configuration
+  it("should respond with OK and the pre-signed data in the body", async () => {
+    // GIVEN a region & a bucketName have been configured
     const givenRegion = "foo";
-    //@ts-ignore
-    jest.spyOn(config, "getUploadBucketRegion").mockReturnValue(givenRegion);
-    // AND a given bucket name from the configuration
     const givenBucketName = "bar";
-    //@ts-ignore
+    jest.spyOn(config, "getUploadBucketRegion").mockReturnValue(givenRegion);
     jest.spyOn(config, "getUploadBucketName").mockReturnValue(givenBucketName);
-    // AND a given random
+    // AND a randomKey
     const givenRandomKey = "baz";
     (randomUUID as jest.Mock).mockReturnValue(givenRandomKey);
-
     // AND the awsSDKServiceModule will return some pre-signed post data
     const givenPreSignedPost = {foo: "foo"} as any
     jest.spyOn(awsSDKServiceModule, "s3_getPresignedPost").mockResolvedValue(givenPreSignedPost);
-
-    // AND the transform() will transform the post data
+    // AND the transform() function will transform the post data
     const givenPresignedResponse = {bar: "bar"} as any
     const transformSpy = jest.spyOn(transformModule, "transformPresignedPostDataToResponse").mockReturnValue(givenPresignedResponse);
 
     // WHEN getPreSigned() is called
     const actualResponse = await handlerModule.getPreSigned();
 
-    // THEN it should call the awsSDKServiceModule.s3_getPresignedPost()
-    // with the given region, bucket name, random key, max file size and expires
+    // THEN expect that the getPreSigned function has called the awsSDKServiceModule.s3_getPresignedPost()
+    // with the given region, bucket name, random key, MAX_FILE_SIZE and EXPIRES
     expect(awsSDKServiceModule.s3_getPresignedPost).toHaveBeenCalledWith(givenRegion, givenBucketName, givenRandomKey, MAX_FILE_SIZE, EXPIRES);
 
-    // AND it should call the transformModule.transformPostData() with the given pre-signed post data
+    // AND has called the transformModule.transformPostData() with the given pre-signed post data
     expect(transformModule.transformPresignedPostDataToResponse).toHaveBeenCalledWith(givenPreSignedPost, givenRandomKey);
 
-    // AND expect to respond with status CREATED
+    // AND it responds with OK status code
     expect(actualResponse.statusCode).toEqual(StatusCodes.OK);
-    // AND expect the handler to return the correct headers
+    // AND the response should have the correct headers
     expect(actualResponse.headers).toMatchObject({"Content-Type": "application/json"});
-    // AND expect the handler to return the result of the transformation function
+    // AND the response body has the result of the transformation function
     expect(JSON.parse(actualResponse.body)).toMatchObject(transformSpy.mock.results[0].value);
   });
 
@@ -110,14 +91,13 @@ describe('test getPreSigned()', () => {
     const mockEvent: APIGatewayProxyEvent = {
       httpMethod: HTTP_VERBS.GET,
     } as any;
-
-    // AND awsSDKServiceModule throws an error
+    // AND awsSDKServiceModule.s3_getPresignedPost() will throw an error
     jest.spyOn(awsSDKServiceModule, "s3_getPresignedPost").mockRejectedValue(new Error("foo"));
 
-    // WHEN the main handler is called with that event
-    const result = await handlerModule.handler(mockEvent);
+    // WHEN the main handler is called with the given  event
+    const actualResponse = await handlerModule.handler(mockEvent);
 
-    // THEN it should return a 500 response
-    expect(result.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+    // THEN expect the actual response to be INTERNAL_SERVER_ERROR
+    expect(actualResponse).toEqual(STD_ERRORS_RESPONSES.INTERNAL_SERVER_ERROR);
   });
 });
