9ec559b112b1f0ad8bfd05e0c696c13ca146058d
rav3n11 <contact.bereket.terefe@gmail.com>
2023-08-18 14:54:40 +0300

    fix!: - Updated var declarations to const : https://sonarcloud.io/project/issues?open=AYfgmm8zhR8SyxmgyCXn&id=tabiya-tech_taxonomy-model-application

--- iac/common/src/urlRewrite.js
+++ iac/common/src/urlRewrite.js
@@ -1,8 +1,8 @@
 function handler(event) {
   // This  function is for a viewer request event trigger.
   // Choose viewer request for event trigger when you associate this function with a distribution.
-  var request = event.request;           // extract the request object
-  var uri = request.uri;                 // extract the URI
+  const request = event.request;           // extract the request object
+  const uri = request.uri;                 // extract the URI
   if (uri.startsWith('/app/')) {
     request.uri = uri.replace(/^\/app\//,'/');
   }
