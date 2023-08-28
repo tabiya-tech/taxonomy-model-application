81d78a3d04b766309deb3b4913c3da283524e96a
Merci Jacob <mercihabam@gmail.com>
2023-08-28 18:15:35 +0200

    feat: write test cases for the models' loading state

--- frontend/src/modeldirectory/components/modelTables/ModelsTable.test.tsx
+++ frontend/src/modeldirectory/components/modelTables/ModelsTable.test.tsx
@@ -1,6 +1,7 @@
 import {render, screen, within} from "@testing-library/react";
 import ModelsTable, {DATA_TEST_ID, TEXT} from "./ModelsTable";
 import {getRandomModels} from "./_test_utilities/mockModelData";
+import {ModelDirectoryTypes} from "../../modelDirectory.types";
 
 function encodeHtmlAttribute(value: string) {
   const element = document.createElement('div');
@@ -83,5 +84,66 @@ describe("ModelsTable", () => {
     // AND the table should not have any row
     const modelTableRows = screen.queryAllByTestId(DATA_TEST_ID.MODEL_TABLE_DATA_ROW);
     expect(modelTableRows).toHaveLength(0);
-  })
-})
+  });
+  
+  test("should render the table with the loader component when the loading property is set to true", () => {
+    // GIVEN  isLoading property is true
+    const givenIsLoading = true;
+    // AND an empty models list is provided
+    const givenModels : ModelDirectoryTypes.ModelInfo[] = [];
+    
+    // WHEN the ModelsTable component is rendered with the given properties
+    render(<ModelsTable models={givenModels} isLoading={givenIsLoading} />)
+    
+    // THEN expect the table to be shown
+    const tableElement = screen.getByTestId(DATA_TEST_ID.MODELS_TABLE_ID);
+    expect(tableElement).toBeInTheDocument();
+    // AND the table to have a header row
+    const actualModelTableHeaderRow = screen.getByTestId(DATA_TEST_ID.MODEL_TABLE_HEADER_ROW);
+    expect(actualModelTableHeaderRow).toBeInTheDocument();
+    // AND the loader component to be shown
+    const modelsLoaderElement = screen.getByTestId(DATA_TEST_ID.MODELS_LOADER);
+    expect(modelsLoaderElement).toBeInTheDocument();
+    // AND no model should be shown
+    const modelTableDataRowElement = screen.queryByTestId(DATA_TEST_ID.MODEL_TABLE_DATA_ROW);
+    expect(modelTableDataRowElement).not.toBeInTheDocument();
+  });
+  
+  test("should render the table without the loader component when the loading property is false", () => {
+    // GIVEN the loading property is false
+    const givenIsLoading = false;
+    // AND n models are provided
+    const givenModels : ModelDirectoryTypes.ModelInfo[] = getRandomModels(3);
+    
+    // WHEN the ModelsTable component is rendered with the given properties
+    render(<ModelsTable models={givenModels} isLoading={givenIsLoading} />);
+    
+    // THEN expect the table to be shown
+    const tableElement = screen.getByTestId(DATA_TEST_ID.MODELS_TABLE_ID);
+    expect(tableElement).toBeInTheDocument();
+    // AND the table to have a header row
+    const actualModelTableHeaderRow = screen.getByTestId(DATA_TEST_ID.MODEL_TABLE_HEADER_ROW);
+    expect(actualModelTableHeaderRow).toBeInTheDocument();
+    // AND the given models to be shown
+    const modelTableDataRowElements = screen.getAllByTestId(DATA_TEST_ID.MODEL_TABLE_DATA_ROW);
+    expect(modelTableDataRowElements).toHaveLength(givenModels.length);
+    // AND the loader component to not be shown
+    const modelsLoaderElement = screen.queryByTestId(DATA_TEST_ID.MODELS_LOADER);
+    expect(modelsLoaderElement).not.toBeInTheDocument();
+  });
+  
+  test("should not render the loader component when the table contains some models", () =>{
+    // GIVEN n models are provided
+    const givenModels = getRandomModels(3);
+    
+    // WHEN the ModelsTable component is rendered with the given models
+    render(<ModelsTable models={givenModels} />);
+    
+    // THEN expect the given models to be shown
+    const modelTableDataRowElements = screen.getAllByTestId(DATA_TEST_ID.MODEL_TABLE_DATA_ROW);
+    expect(modelTableDataRowElements).toHaveLength(givenModels.length);
+    // AND the loader component to not be shown
+    const modelsLoaderElement = screen.queryByTestId(DATA_TEST_ID.MODELS_LOADER);
+    expect(modelsLoaderElement).not.toBeInTheDocument();
+  });
+});
