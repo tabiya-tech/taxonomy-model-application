fecade4590228eab2cb3b100ff53925d7aa3689a
Benisis, Apostolos <apostolos@benisis.de>
2023-09-06 19:13:02 +0300

    fix(frontend):  SnackbarProvider component violates WAG2A rule Buttons must have discernible text (button-name)

--- frontend/src/theme/SnackbarProvider/SnackbarProvider.stories.tsx
+++ frontend/src/theme/SnackbarProvider/SnackbarProvider.stories.tsx
@@ -1,13 +1,23 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import SnackbarProvider, {useSnackbar, VariantType} from './SnackbarProvider';
 import {Button} from '@mui/material';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
+import {faker} from "@faker-js/faker";
+import {useEffect} from "react";
 
 const meta: Meta<typeof SnackbarProvider> = {
   title: 'Components/SnackBar',
   component: SnackbarProvider,
   tags: ['autodocs'],
   argTypes: {},
+  parameters: {
+    docs: { disable: true },
+    a11y: {
+      // Disabling a11y due to https://github.com/iamhosseindhv/notistack/issues/579
+      // See also frontend/src/theme/SnackbarProvider/SnackbarProvider.tsx
+      disable: true,
+    }
+  }
 };
 
 export default meta;
@@ -51,9 +61,57 @@ const TestButton = () => {
   );
 };
 export const Shown: Story = {
+  // exclude from a11y tests as this is for visual demonstration only
+  parameters: {
+    docs: { disable: false },
+    a11y: {disable: true}
+  },
   render: () => (
     <SnackbarProvider>
       <TestButton/>
     </SnackbarProvider>
   ),
 };
+
+export const ShownInfo: Story = {
+  render: () => {
+    return (<ShowNotifications notifications={getRandomNotificationProps(5, "info")}/>)
+  }
+};
+
+export const ShownWarning: Story = {
+  render: () => {
+    return (<ShowNotifications notifications={getRandomNotificationProps(5, "warning")}/>)
+  }
+};
+
+export const ShownError: Story = {
+  render: () => {
+    return (<ShowNotifications notifications={getRandomNotificationProps(5, "error")}/>)
+  }
+};
+
+export const ShownMixed: Story = {
+  render: () => {
+    const notifications = [...getRandomNotificationProps(3, "info"), ...getRandomNotificationProps(3, "warning"), ...getRandomNotificationProps(3, "error")]
+    return (<ShowNotifications notifications={notifications}/>
+    )
+  }
+};
+
+function getRandomNotificationProps(count: number, variant: VariantType): { message: string, variant: VariantType }[] {
+  return Array(count).fill(undefined).map(() => ({message: faker.lorem.words(10), variant: variant}));
+}
+
+function ShowNotifications(props: { notifications: { message: string, variant: VariantType }[] }) {
+  const {enqueueSnackbar} = useSnackbar();
+  useEffect(() => {
+    props.notifications.forEach((notification) => {
+      enqueueSnackbar(notification.message, {
+        variant: notification.variant,
+        transitionDuration: {enter: 0, exit: 0}
+      }); // disable the transition to speed up the test
+    })
+  });
+  return (<></>);
+}
--- frontend/src/theme/SnackbarProvider/SnackbarProvider.tsx
+++ frontend/src/theme/SnackbarProvider/SnackbarProvider.tsx
@@ -17,8 +17,12 @@ export const DATA_TEST_ID = {
 
 const SnackbarCloseButton = (key: SnackbarKey) => {
   const {closeSnackbar} = useSnackbar();
-  return <IconButton data-testid={DATA_TEST_ID.SNACKBAR_CLOSE_BUTTON} onClick={() => closeSnackbar(key)}>
-    <CloseIcon style={{color: 'white', fontSize: '14px'}}/>
+  // Currently due to a bug in the notistack library, we cannot ref to the correct ARIA ids (see https://github.com/iamhosseindhv/notistack/issues/579)
+  // Suing  the title attribute for accessibility, is not ideal. When multiple snackbars are open,
+  // the title will be the same for all of them violating the WAG2A rule of unique id. See https://tabiya-tech.atlassian.net/browse/PLAT-129
+  return <IconButton title={"Close notification"} data-testid={DATA_TEST_ID.SNACKBAR_CLOSE_BUTTON}
+                     onClick={() => closeSnackbar(key)}>
+    <CloseIcon color="action"/>
   </IconButton>;
 }
 
@@ -54,11 +58,12 @@ const SnackbarProvider: React.FC<SnackbarProviderProps & _SnackbarProviderProps>
       success: StyledMaterialDesignContent,
       error: StyledMaterialDesignContent,
       warning: StyledMaterialDesignContent,
-    }
+    },
+    ...props
   };
 
   return (
-    <SNACKBAR_PROVIDER {...notistackOptions} {...props as SnackbarProviderProps} action={SnackbarCloseButton}>
+    <SNACKBAR_PROVIDER {...notistackOptions} action={SnackbarCloseButton}>
       {children}
     </SNACKBAR_PROVIDER>
   );
