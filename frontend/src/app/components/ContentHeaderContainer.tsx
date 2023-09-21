ee3e6f0a876c1b0435ac1a58e18fe789064dd66f
luc <luc.bayo@gmail.com>
2023-09-21 12:47:14 +0200

    feat: add app layout section color

--- frontend/src/app/components/ContentHeaderContainer.tsx
+++ frontend/src/app/components/ContentHeaderContainer.tsx
@@ -1,15 +1,9 @@
-import {useAppLayout} from '../AppLayoutProvider';
-import {Box} from "@mui/material";
+import { useAppLayout } from '../AppLayoutProvider';
+import { Box } from '@mui/material';
 
 const ContentHeaderContainer = () => {
-  const {contentHeader} = useAppLayout()
-  return <>
-    <Box
-      sx={{ bgcolor: 'secondary.main' }}
-      padding={2}
-    >{contentHeader}
-    </Box>
-  </>
+  const { contentHeader } = useAppLayout();
+  return<Box padding={2}>{contentHeader}</Box>;
 };
 
 export default ContentHeaderContainer;
