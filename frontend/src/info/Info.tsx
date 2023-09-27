f46ab8b1d80593e04c46a1897a7497fc06ec6db7
Fides <fniragena@gmail.com>
2023-09-27 17:50:25 +0200

    feat: redesign the application info page

--- frontend/src/info/Info.tsx
+++ frontend/src/info/Info.tsx
@@ -1,46 +1,68 @@
 import React, {useEffect, useState, useMemo} from 'react';
 import {InfoProps} from './info.types';
 import InfoService from './info.service';
-import {Box, CircularProgress, FormLabel, List, ListItem, ListItemText} from "@mui/material";
+import {Box, Skeleton, Typography} from "@mui/material";
+import ContentTitle from "src/theme/ContentTitle";
+
 
 const uniqueId = "37d307ae-4f1e-4d8d-bafe-fd642f8af4dc"
 export const DATA_TEST_ID = {
-  INFO_ROOT: `version-${uniqueId}`,
-  VERSION_FRONTEND_ROOT: `version-frontend-${uniqueId}`,
-  VERSION_BACKEND_ROOT: `version-backend-${uniqueId}`,
-}
+    INFO_ROOT: `version-${uniqueId}`,
+    VERSION_FRONTEND_ROOT: `version-frontend-${uniqueId}`,
+    VERSION_BACKEND_ROOT: `version-backend-${uniqueId}`,
+};
+
+const VersionInfoItem = ({ title, value, skeleton }: {title: string, value: string, skeleton?: boolean}) => {
+    return (
+      <Box>
+        <Typography variant="h6">{title}</Typography>
+        <Typography variant="body1" color="text.primary">
+          {skeleton ? <Skeleton>{value.replace(/./g,'\u00A0\u00A0')}</Skeleton> : value}
+        </Typography>
+      </Box>
+    );
+};
+
+const VersionContainer = ({ dataTestId, title, info }: {dataTestId: string, title: string, info: InfoProps}) => {
+    return (
+        <Box display="flex" gap="30px" data-testid={dataTestId}>
+            <Typography variant="h6">{title}</Typography>
+            {info ? (
+                <Box display="flex" flexDirection="column" gap="12px">
+                    <VersionInfoItem title="Date" value={info.date} />
+                    <VersionInfoItem title="Branch" value={info.branch} />
+                    <VersionInfoItem title="Build Number" value={info.buildNumber} />
+                    <VersionInfoItem title="GIT SHA" value={info.sha} />
+                </Box>
+            ) : (
+                <Box display="flex" flexDirection="column" gap="12px">
+                    <VersionInfoItem title="Date" value={"0000-00-00T00:00:00.000Z"} skeleton={true}/>
+                    <VersionInfoItem title="Branch" value={"foo"} skeleton={true}/>
+                    <VersionInfoItem title="Build Number" value={"000"} skeleton={true}/>
+                    <VersionInfoItem title="GIT SHA" value={"foofoofoofoofoofoofoofoofoofoofoofoofoo"} skeleton={true}/>
+                </Box>
+            )}
+        </Box>
+    );
+};
 
 const Info = () => {
-  const [version, setVersions] = useState<InfoProps[]>([]);
-  const infoService = useMemo(() => new InfoService(), []);
+    const [version, setVersions] = useState<InfoProps[]>([]);
+    const infoService = useMemo(() => new InfoService(), []);
 
-  useEffect(() => {
-    infoService.loadInfo().then((data) => setVersions(data));
-  }, [infoService]);
-  return (
-    <div data-testid={DATA_TEST_ID.INFO_ROOT}>
-      <div id="frontend" data-testid={DATA_TEST_ID.VERSION_FRONTEND_ROOT}>
-        {RenderVersion("Frontend", `${uniqueId}-frontend`, version[0])}
-      </div>
-      <div id="backend" data-testid={DATA_TEST_ID.VERSION_BACKEND_ROOT}>
-        {RenderVersion("Backend", `${uniqueId}-backend`, version[1])}
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        infoService.loadInfo().then((data) => setVersions(data));
+    }, [infoService]);
+
+    return (
+        <Box data-testid={DATA_TEST_ID.INFO_ROOT} display="flex" flexDirection="column" gap="28px">
+            <ContentTitle text="Info" />
+            <Box display="flex" flexDirection="column" gap="40px">
+                <VersionContainer title="Frontend" info={version[0]} dataTestId={DATA_TEST_ID.VERSION_FRONTEND_ROOT} />
+                <VersionContainer title="Backend" info={version[1]} dataTestId={DATA_TEST_ID.VERSION_BACKEND_ROOT} />
+            </Box>
+        </Box>
+    );
 };
 
 export default Info;
-
-const RenderVersion = (title: string, uniqueId: string, info: InfoProps) => {
-  return <Box sx={{width: 500}} component="fieldset">
-    <legend><FormLabel id={uniqueId}>{title}</FormLabel></legend>
-    {info ? <List dense={true}>
-        <ListItem><FormLabel>Date: </FormLabel><ListItemText primary={`${info.date}`}/></ListItem>
-        <ListItem><FormLabel>Branch: </FormLabel><ListItemText primary={`${info.branch}`}/></ListItem>
-        <ListItem><FormLabel>Build Number: </FormLabel><ListItemText primary={`${info.buildNumber}`}/></ListItem>
-        <ListItem><FormLabel>GIT SHA:</FormLabel><ListItemText primary={`${info.sha}`}/></ListItem>
-      </List>
-      : <CircularProgress aria-labelledby={uniqueId}/>
-    }
-  </Box>;
-};
