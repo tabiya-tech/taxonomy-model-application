97dac26014c586fdfa1750b800d65123313d2a37
luc <luc.bayo@gmail.com>
2023-09-29 17:44:09 +0200

    - chore: add option to extend the mui theme
    REVIEW:
    - added comments to explain the usage of tabiyaSpacing and tabiyaRounding
    
    - explicitly added shape.borderRadius: 8 in the applicationTheme
    
    - adjusted the AppSidebar as the first component to use the tabiyaRounding and streamlined it's style to the theme

--- frontend/src/theme/applicationTheme.ts
+++ frontend/src/theme/applicationTheme.ts
@@ -1,25 +1,46 @@
-import {createTheme} from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const applicationTheme = createTheme({
   palette: {
     primary: {
-      main: '#002147',
+      main: "#002147",
     },
     secondary: {
-      main: '#EEFF41',
-      light: '#5CFF9F',
+      main: "#EEFF41",
+      light: "#5CFF9F",
     },
     text: {
-      primary: '#002147'
+      primary: "#002147",
     },
     common: {
-      white: '#ffffff',
-      black: '#000000',
-    }
+      white: "#ffffff",
+      black: "#000000",
+    },
+  },
+  spacing: 8,
+  tabiyaSpacing: {
+    none: 0,
+    xs: 0.5,
+    sm: 1,
+    md: 2,
+    lg: 3,
+    xl: 4,
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  tabiyaRounding: {
+    none: 0,
+    xs: 0.5,
+    sm: 1,
+    md: 2,
+    lg: 3,
+    xl: 4,
+    full: "50%",
   },
   typography: {
     htmlFontSize: 16, // Set the base font size
-    fontFamily: 'Inter, sans-serif', // Set the desired font family
+    fontFamily: "Inter, sans-serif", // Set the desired font family
     fontSize: 16, // Set the base font size
     // Define additional typography styles if needed
   },
@@ -28,7 +49,7 @@ const applicationTheme = createTheme({
       styleOverrides: {
         asterisk: {
           color: "red",
-        }
+        },
       },
     },
     MuiInput: {
@@ -37,28 +58,28 @@ const applicationTheme = createTheme({
           fontSize: "1rem",
           "::placeholder": {
             color: "gray",
-          }
-        }
-      }
+          },
+        },
+      },
     },
     MuiButton: {
       defaultProps: {
-        variant: 'outlined',
+        variant: "outlined",
       },
       styleOverrides: {
         root: {
-          textTransform: 'none',
-        }
-      }
+          textTransform: "none",
+        },
+      },
     },
     MuiTableHead: {
       defaultProps: {
         style: {
-          background: "#dedede"
-        }
-      }
-    }
-  }
+          background: "#dedede",
+        },
+      },
+    },
+  },
 });
 
 export default applicationTheme;
