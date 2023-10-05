f1bb841d99596cfb76a2ca1137e6b0d04ca6137f
rav3n11 <contact.bereket.terefe@gmail.com>
2023-10-05 14:19:25 +0300

    feat: Showcase the mui theme in storybook

--- frontend/src/theme/applicationTheme.stories.tsx
+++ frontend/src/theme/applicationTheme.stories.tsx
@@ -1,8 +1,11 @@
 import { Meta, StoryObj } from "@storybook/react";
-import {Box, Typography} from "@mui/material";
+import { Box, rgbToHex, Typography, useTheme } from "@mui/material";
+import { Palette, PaletteColor, Theme } from "@mui/material/styles";
+import { TabiyaSize } from "./theme";
+import Paper from "@mui/material/Paper";
 
 const meta: Meta = {
-  title: "Application/Style",
+  title: "Components/Style",
   component: Box,
   tags: ["autodocs"],
   argTypes: {},
@@ -11,39 +14,378 @@ const meta: Meta = {
 export default meta;
 
 type Story = StoryObj;
+
+const colorCategories = [
+  "primary",
+  "secondary",
+  "error",
+  "warning",
+  "info",
+  "success",
+  "tabiyaYellow",
+  "tabiyaGreen",
+  "containerBackground",
+] as const;
+type ColorCategory = (typeof colorCategories)[number];
+
+const TypographyElements = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        padding: theme.tabiyaSpacing.lg,
+        gap: theme.tabiyaSpacing.lg,
+      }}
+    >
+      <Typography variant={"h4"}>Basic Typography</Typography>
+      <Box
+        sx={{
+          gap: theme.tabiyaSpacing.md,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "start",
+        }}
+      >
+        {Object.keys(theme.typography as Record<string, any>).map(
+          (typographyKey: string) => {
+            const typographyVariant =
+              theme.typography[typographyKey as keyof typeof theme.typography];
+            if (
+              typeof typographyVariant === "object" &&
+              typographyVariant !== null &&
+              "fontFamily" in typographyVariant &&
+              "fontSize" in typographyVariant &&
+              typographyKey !== "inherit"
+            ) {
+              return (
+                <Typography variant={typographyKey as any} key={typographyKey}>
+                  {`<${typographyKey}> ${typographyVariant.fontFamily} ${typographyVariant.fontSize}`}
+                </Typography>
+              );
+            }
+            return null;
+          }
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+const PaletteElements = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: theme.tabiyaSpacing.lg,
+      }}
+    >
+      <Typography variant={"h4"}> Basic Colors</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          alignItems: "start",
+          padding: theme.tabiyaSpacing.lg,
+          gap: theme.tabiyaSpacing.md,
+        }}
+      >
+        {colorCategories.map((category) => (
+          <Box
+            key={category}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: theme.tabiyaSpacing.sm,
+              gap: theme.tabiyaSpacing.md,
+            }}
+          >
+            <Typography variant={"subtitle1"}>{category}</Typography>
+            <Box>
+              <ColorBox theme={theme} category={category} variant={"main"} />
+              <ColorBox theme={theme} category={category} variant={"light"} />
+              <ColorBox theme={theme} category={category} variant={"dark"} />
+            </Box>
+          </Box>
+        ))}
+      </Box>
+      <Box>
+        <Typography variant={"h4"}>GreyScale</Typography>
+        <Box
+          sx={{ padding: theme.tabiyaSpacing.lg, gap: theme.tabiyaSpacing.md }}
+        >
+          {Object.entries(theme.palette.grey).map(([shade, color]) => (
+            <ColorBox
+              key={shade}
+              shade={shade as keyof Palette["grey"]}
+              theme={theme}
+              color={color}
+            />
+          ))}
+        </Box>
+      </Box>
+      <Box>
+        <Typography variant={"h4"}>Text Colors</Typography>
+        <Box
+          sx={{ padding: theme.tabiyaSpacing.lg, gap: theme.tabiyaSpacing.md }}
+        >
+          {Object.entries(theme.palette.text).map(([variant, color]) => (
+            <ColorBox
+              theme={theme}
+              color={
+                variant === "textWhite"
+                  ? theme.palette.primary.main
+                  : theme.palette.common.white
+              }
+              key={variant}
+            >
+              <Typography variant={"subtitle1"} color={color}>
+                {variant} : {color}
+              </Typography>
+            </ColorBox>
+          ))}
+        </Box>
+      </Box>
+      <Box>
+        <Typography variant={"h4"}>Common Colors</Typography>
+        <Box
+          sx={{ padding: theme.tabiyaSpacing.lg, gap: theme.tabiyaSpacing.md }}
+        >
+          {Object.entries(theme.palette.common).map(([variant, color]) => (
+            <ColorBox
+              theme={theme}
+              color={
+                variant === "white"
+                  ? theme.palette.common.black
+                  : theme.palette.common.white
+              }
+              key={variant}
+            >
+              <Typography variant={"subtitle1"} color={color}>
+                {variant} : {color}
+              </Typography>
+            </ColorBox>
+          ))}
+        </Box>
+      </Box>
+      ;
+    </Box>
+  );
+};
+
+const SpacingAndRoundingElements = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: theme.tabiyaSpacing.md,
+      }}
+    >
+      <SpacingElements theme={theme} />
+      <RoundingElements theme={theme} />
+    </Box>
+  );
+};
+
+interface SpacingAndRoundingElementsProps {
+  theme: Theme;
+}
+
+const SpacingElements = (props: SpacingAndRoundingElementsProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: props.theme.tabiyaSpacing.md,
+      }}
+    >
+      <Typography variant={"h4"}>Spacing</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: props.theme.tabiyaSpacing.md,
+        }}
+      >
+        {Object.entries(props.theme.tabiyaSpacing).map(([key, value]) => (
+          <Box
+            key={key}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "20rem",
+              height: "20rem",
+              backgroundColor: props.theme.palette.containerBackground.main,
+              border: "2px solid",
+              borderColor: props.theme.palette.secondary.main,
+              position: "relative",
+            }}
+          >
+            <Box
+              sx={{
+                margin: props.theme.tabiyaSpacing[key as keyof TabiyaSize],
+              }}
+            >
+              <Paper
+                sx={{
+                  height: "10rem",
+                  padding: props.theme.tabiyaSpacing.md,
+                }}
+              >
+                <Typography variant={"subtitle1"}>
+                  With margin: props.theme.tabiyaSpacing.
+                  {key as keyof TabiyaSize}
+                </Typography>
+              </Paper>
+            </Box>
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                backgroundColor: props.theme.palette.primary.main,
+                color: props.theme.palette.primary.contrastText,
+                padding: "0.5rem",
+              }}
+            >
+              {`${key}: ${value} * ${props.theme.spacing(1)}`}
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+const RoundingElements = (props: SpacingAndRoundingElementsProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: props.theme.tabiyaSpacing.md,
+      }}
+    >
+      <Typography variant={"h4"}>Rounding</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: props.theme.tabiyaSpacing.md,
+        }}
+      >
+        {Object.entries(props.theme.tabiyaRounding).map(([key, value]) => (
+          <Box
+            key={key}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "12rem",
+              height: "12rem",
+              backgroundColor: props.theme.palette.containerBackground.main,
+              border: "2px solid",
+              borderColor: props.theme.palette.secondary.main,
+              position: "relative",
+            }}
+          >
+            <Paper
+              sx={{
+                width: "10rem",
+                height: "10rem",
+                borderRadius: value,
+              }}
+            ></Paper>
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                backgroundColor: props.theme.palette.primary.main,
+                color: props.theme.palette.primary.contrastText,
+                padding: "0.5rem",
+              }}
+            >
+              {`${key}: ${value} ${
+                key !== "full" ? "*" + props.theme.spacing(1) : ""
+              }`}
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+interface ColorBoxProps {
+  theme: Theme;
+  category?: ColorCategory;
+  variant?: keyof PaletteColor;
+  color?: string;
+  shade?: keyof Palette["grey"];
+  children?: any;
+}
+
+const ColorBox = (props: ColorBoxProps) => {
+  let color: string;
+  let contrastText: string;
+
+  if (props.color) {
+    color = rgbToHex(props.color);
+    contrastText = props.theme.palette.getContrastText(color);
+  } else if (props.category && props.variant) {
+    color = rgbToHex(props.theme.palette[props.category][props.variant]);
+    contrastText = props.theme.palette[props.category].contrastText;
+  } else if (props.shade) {
+    color = rgbToHex(props.theme.palette.grey[props.shade]);
+    contrastText = props.theme.palette.getContrastText(color);
+  } else {
+    throw new Error("Invalid props provided to ColorBox");
+  }
+
+  return (
+    <Box
+      sx={{
+        height: "4rem",
+        width: "16rem",
+        backgroundColor: color,
+        color: contrastText,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "start",
+        padding: props.theme.tabiyaSpacing.md,
+      }}
+    >
+      {props.children ? props.children : props.variant ?? props.shade ?? color}
+    </Box>
+  );
+};
+
 export const TypographyStyles: Story = {
   args: {
-    children: (
-      <>
-        <Typography variant={"h1"}>{"<h1> This is a test"}</Typography>
-        <Typography variant={"h2"}>{"<h2> This is a test"}</Typography>
-        <Typography variant={"h3"}>{"<h3> This is a test"}</Typography>
-        <Typography variant={"h4"}>{"<h4> This is a test"}</Typography>
-        <Typography variant={"h5"}>{"<h5> This is a test"}</Typography>
-        <Typography variant={"h6"}>{"<h6> This is a test"}</Typography>
-        <Typography variant={"subtitle1"}>
-          {"<subtitle1> This is a test"}
-        </Typography>
-        <Typography variant={"subtitle2"}>
-          {"<subtitle2> This is a test"}
-        </Typography>
-        <Typography variant={"body1"}>{"<body1> This is a test"}</Typography>
-        <Typography variant={"body2"}>{"<body2> This is a test"}</Typography>
-        <Typography variant={"body1"}>
-          <Typography variant={"button"}>{"<button> This is a test"}</Typography>
-        </Typography>
-        <Typography sx={{lineHeight:"0"}}>
-          <Typography variant={"caption"}>
-            {"<caption> This is a test"}
-          </Typography>
-        </Typography>
+    children: <TypographyElements />,
+  },
+};
 
-        <Typography sx={{lineHeight:"0"}}>
-          <Typography variant={"overline"}>
-            {"<overline> This is a test"}
-          </Typography>
-        </Typography>
-      </>
-    ),
+export const PaletteStyles: Story = {
+  args: {
+    children: <PaletteElements />,
+  },
+};
+
+export const SpacingAndRoundingStyles: Story = {
+  args: {
+    children: <SpacingAndRoundingElements />,
   },
 };
