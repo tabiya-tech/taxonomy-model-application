74def7b6df57f87ae65f55545b4930407b166d08
Merci Jacob <mercihabam@gmail.com>
2023-10-11 12:45:56 +0200

    fix: remove settings and language icons from the AppHeader.tsx

--- frontend/src/app/components/AppHeader.test.tsx
+++ frontend/src/app/components/AppHeader.test.tsx
@@ -2,7 +2,7 @@
 import "src/_test_utilities/consoleMock";
 
 import AppHeader, { DATA_TEST_ID } from "./AppHeader";
-import { render, screen, within } from "src/_test_utilities/test-utils";
+import { render, screen } from "src/_test_utilities/test-utils";
 import { HashRouter } from "react-router-dom";
 import { routerPaths } from "src/app/routerConfig";
 
@@ -31,33 +31,9 @@ describe("AppHeader render tests", () => {
     // AND the header to be shown
     const appHeaderContainer = screen.getByTestId(DATA_TEST_ID.APP_HEADER_CONTAINER);
     expect(appHeaderContainer).toBeInTheDocument();
-    // AND the icons container to be shown
-    const appHeaderIconsContainer = screen.getByTestId(DATA_TEST_ID.APP_HEADER_ICONS_CONTAINER);
+    // AND the user icon to be shown
+    const appHeaderIconsContainer = screen.getByTestId(DATA_TEST_ID.APP_HEADER_ICON_USER);
     expect(appHeaderIconsContainer).toBeInTheDocument();
-  });
-
-  test.each([
-    ["Settings", DATA_TEST_ID.APP_HEADER_ICON_SETTINGS],
-    ["Language", DATA_TEST_ID.APP_HEADER_ICON_LANGUAGE],
-    ["User", DATA_TEST_ID.APP_HEADER_ICON_USER],
-  ])(`renders the %s icon with the icons container`, (iconName, dataTestId) => {
-    // GIVEN a AppHeader component that has access to the router
-    const givenAppHeader = (
-      <HashRouter>
-        <AppHeader />
-      </HashRouter>
-    );
-
-    // WHEN it is rendered
-    render(givenAppHeader);
-
-    /// THEN expect no errors or warning to have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-    // AND the respective icon to be shown within the icons container
-    const appHeaderIconsContainer = screen.getByTestId(DATA_TEST_ID.APP_HEADER_ICONS_CONTAINER);
-    const actualIcon = within(appHeaderIconsContainer).getByTestId(dataTestId);
-    expect(actualIcon).toBeInTheDocument();
   });
 });
 
