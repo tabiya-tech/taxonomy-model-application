2418ea21a7d3054a8f94d2a78303cdf270df17f5
luc <luc.bayo@gmail.com>
2023-10-18 15:39:23 +0200

    feat(frontend): listen to online and offline status and show warning

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,8 +1,43 @@
+import { useEffect } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import routerConfig from "./routerConfig";
 import { AppLayout } from "./components";
+import { useSnackbar } from "src/theme/SnackbarProvider/SnackbarProvider";
+
+const uniqueId = "8490f1cc-0786-476f-821e-34b6b712d63f";
+
+export const SNACKBAR_KEYS = {
+  OFFLINE_ERROR: `offline-error-${uniqueId}`,
+};
 
 export const TaxonomyModelApp = () => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    console.log("TaxonomyModelApp: useEffect");
+    const handleOnline = () => {
+      closeSnackbar(SNACKBAR_KEYS.OFFLINE_ERROR);
+    };
+
+    const handleOffline = () => {
+      enqueueSnackbar(`You are offline`, {
+        variant: "warning",
+        key: SNACKBAR_KEYS.OFFLINE_ERROR,
+        preventDuplicate: true,
+        persist: true,
+      });
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+    return () => {
+      console.log("TaxonomyModelApp: cleanup");
+
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  });
+
   return (
     <HashRouter>
       <AppLayout>
