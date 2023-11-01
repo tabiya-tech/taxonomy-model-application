2dd8a2aaaaf8f893f526b70b486a2c2522734940
Merci Jacob <mercihabam@gmail.com>
2023-11-01 14:37:56 +0200

    chore(api-specs): upgrade to typescript version 4.9.5

--- api-specifications/src/error/types.ts
+++ api-specifications/src/error/types.ts
@@ -3,6 +3,13 @@ import ModelInfo from "../modelInfo";
 import ErrorConstants from "./constants";
 
 namespace ErrorTypes {
+  // This is here to make sure the namespace is not empty and the:
+  //    "Cannot use 'export import' on a type or type-only namespace when the '--isolatedModules' flag is provided"
+  // error is not thrown.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _ = 0;
+  // ---
+
   export interface Payload {
     errorCode:
       | ErrorConstants.ErrorCodes
--- api-specifications/src/importProcessState/types.ts
+++ api-specifications/src/importProcessState/types.ts
@@ -1,6 +1,13 @@
 import { ImportProcessState } from "./enums";
 
 namespace ImportProcessStateTypes {
+  // This is here to make sure the namespace is not empty and the:
+  //    "Cannot use 'export import' on a type or type-only namespace when the '--isolatedModules' flag is provided"
+  // error is not thrown.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const _ = 0;
+  // ---
+
   export type Result = {
     errored: boolean;
     parsingErrors: boolean;
