ddc54f3d6e6e85559c6d130cfcf3ba4392828978
Benisis, Apostolos <apostolos@benisis.de>
2023-12-30 22:15:46 +0100

    chore(backend): REST APIs for /models/ should respond with modelInfo entities that have the actual path of the entity in the "path" and "tabiyaPath" properties.

--- backend/src/modelInfo/transform.ts
+++ backend/src/modelInfo/transform.ts
@@ -1,5 +1,7 @@
 import ModelInfoAPISpecs from "api-specifications/modelInfo";
-import { IModelInfo } from "./modelInfo.types";
+import { IModelInfo } from "modelInfo/modelInfo.types";
+import { Routes } from "routes.constant";
+
 export function transform(data: IModelInfo, baseURL: string): ModelInfoAPISpecs.Types.POST.Response.Payload {
   return {
     id: data.id,
@@ -12,8 +14,8 @@ export function transform(data: IModelInfo, baseURL: string): ModelInfoAPISpecs.
     originUUID: data.originUUID,
     previousUUID: data.previousUUID,
     locale: data.locale,
-    path: `${baseURL}/${data.id}`,
-    tabiyaPath: `${baseURL}/${data.UUID}`,
+    path: `${baseURL}${Routes.MODELS_ROUTE}/${data.id}`,
+    tabiyaPath: `${baseURL}${Routes.MODELS_ROUTE}/${data.UUID}`,
     exportProcessState: data.exportProcessState.map((exportProcessState) => ({
       ...exportProcessState,
       timestamp: exportProcessState.timestamp.toISOString(),
