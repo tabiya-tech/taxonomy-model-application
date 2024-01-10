8339ba314964555f60857a98df62d3746a369353
Fides <fniragena@gmail.com>
2024-01-10 11:12:14 +0200

    chore(api-specs): presigned

--- api-specifications/src/presigned/index.test.ts
+++ api-specifications/src/presigned/index.test.ts
@@ -2,17 +2,17 @@ describe("Test the presigned module", () => {
   test("The presigned module can be required via the index", () => {
     //GIVEN the module
     //WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
       expect(() => {
         require("./");
       }).not.toThrowError();
-      const presignedModule = require("./").default;
+      const presignedModule = await import("./");
       // AND the schema should be defined
-      expect(presignedModule.Schemas.GET.Response.Payload).toBeDefined();
+      expect(presignedModule.default.Schemas.GET.Response.Payload).toBeDefined();
 
       // AND the constants should be defined
-      const Constants = presignedModule.Constants;
+      const Constants = presignedModule.default.Constants;
       expect(Constants.EXPIRES).toBeDefined();
       expect(Constants.MAX_FILE_SIZE).toBeDefined();
     }).not.toThrowError();
@@ -21,9 +21,10 @@ describe("Test the presigned module", () => {
   test("The presigned module matches the snapshot", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
-      expect(require("./").default).toMatchSnapshot();
+      const presignedModule = await import("./");
+      expect(presignedModule.default).toMatchSnapshot();
     }).not.toThrowError();
   });
 });
