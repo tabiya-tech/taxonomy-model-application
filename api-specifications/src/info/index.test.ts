d44711baeb247024b15d5e754c93e7f86f0c80b4
Fides <fniragena@gmail.com>
2024-01-10 11:03:21 +0200

    chore(api-specs): info

--- api-specifications/src/info/index.test.ts
+++ api-specifications/src/info/index.test.ts
@@ -2,22 +2,24 @@ describe("Test the info module", () => {
   test("The info module can be required via the index", () => {
     //GIVEN the module
     //WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
       expect(() => {
         require("./");
       }).not.toThrowError();
       // AND check if Schema is defined in it
-      expect(require("./").default.Schemas.GET.Response.Payload).toBeDefined();
+      const infoModule = await import("./");
+      expect(infoModule.default.Schemas.GET.Response.Payload).toBeDefined();
     }).not.toThrowError();
   });
 
   test("The info module matches the snapshot", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
-      expect(require("./").default).toMatchSnapshot();
+      const infoModule = await import("./");
+      expect(infoModule.default).toMatchSnapshot();
     }).not.toThrowError();
   });
 });
