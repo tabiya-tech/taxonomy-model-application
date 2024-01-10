6fd76a918e5761098746de798e1d63a9d18dd2ba
Fides <fniragena@gmail.com>
2024-01-10 10:41:17 +0200

    chore(api-specs): exportProcessState

--- api-specifications/src/exportProcessState/index.test.ts
+++ api-specifications/src/exportProcessState/index.test.ts
@@ -2,25 +2,26 @@ describe("Test the exportProcessState module", () => {
   test("The exportProcessState module can be required via the index", () => {
     //GIVEN the module
     //WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
       expect(() => {
         require("./");
       }).not.toThrowError();
-      const exportProcessStateModule = require("./").default;
+      const exportProcessStateModule = await import("./");
       // AND check if Schema is defined in it
-      expect(exportProcessStateModule.Schemas.GET.Response.Payload).toBeDefined();
+      expect(exportProcessStateModule.default.Schemas.GET.Response.Payload).toBeDefined();
       // AND check if the enums are defined in it
-      expect(exportProcessStateModule.Enums.Status).toBeDefined();
+      expect(exportProcessStateModule.default.Enums.Status).toBeDefined();
     }).not.toThrowError();
   });
 
   test("The export module matches the snapshot", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
-      expect(require("./").default).toMatchSnapshot();
+      const exportProcessStateModule = await import("./");
+      expect(exportProcessStateModule.default).toMatchSnapshot();
     }).not.toThrowError();
   });
 });
