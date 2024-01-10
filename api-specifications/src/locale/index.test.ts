ba5dac3b8b80030012b35a4e00bde929c93e0549
Fides <fniragena@gmail.com>
2024-01-10 11:06:35 +0200

    chore(api-specs): locale

--- api-specifications/src/locale/index.test.ts
+++ api-specifications/src/locale/index.test.ts
@@ -2,27 +2,28 @@ describe("Test the locale module", () => {
   test("The locale module can be required via the index", () => {
     //GIVEN the module
     //WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
       expect(() => {
         require("./");
       }).not.toThrowError();
       // AND check if Schema is defined in it
-      const localeModule = require("./").default;
-      expect(localeModule.Schemas.Payload).toBeDefined();
+      const localeModule = await import("./");
+      expect(localeModule.default.Schemas.Payload).toBeDefined();
 
       // AND check if Constants is defined in it
-      expect(localeModule.Constants.LOCALE_SHORTCODE_MAX_LENGTH).toBeDefined();
-      expect(localeModule.Constants.NAME_MAX_LENGTH).toBeDefined();
+      expect(localeModule.default.Constants.LOCALE_SHORTCODE_MAX_LENGTH).toBeDefined();
+      expect(localeModule.default.Constants.NAME_MAX_LENGTH).toBeDefined();
     }).not.toThrowError();
   });
 
   test("The locale module matches the snapshot", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
-      expect(require("./").default).toMatchSnapshot();
+      const localeModule = await import("./");
+      expect(localeModule.default).toMatchSnapshot();
     }).not.toThrowError();
   });
 });
