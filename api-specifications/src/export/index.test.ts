c4dd9a5d7110f1b2303764eaaf75c83360f542a8
Fides <fniragena@gmail.com>
2024-01-10 10:33:22 +0200

    chore(api-specs): export

--- api-specifications/src/export/index.test.ts
+++ api-specifications/src/export/index.test.ts
@@ -2,24 +2,26 @@ describe("Test the export module", () => {
   test("The export module can be required via the index", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
       expect(() => {
         require("./");
       }).not.toThrowError();
+      const exportModule = await import("./");
       // AND check if the various exports are defined
-      expect(require("./").default.Schemas.POST.Request.Payload).toBeDefined();
-      expect(require("./").default.Constants.MAX_PAYLOAD_LENGTH).toBeDefined();
-      expect(require("./").default.Enums.POST).toBeDefined();
+      expect(exportModule.default.Schemas.POST.Request.Payload).toBeDefined();
+      expect(exportModule.default.Constants.MAX_PAYLOAD_LENGTH).toBeDefined();
+      expect(exportModule.default.Enums.POST).toBeDefined();
     }).not.toThrowError();
   });
 
   test("The export module matches the snapshot", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
-      expect(require("./").default).toMatchSnapshot();
+      const exportModule = await import("./");
+      expect(exportModule.default).toMatchSnapshot();
     }).not.toThrowError();
   });
 });
