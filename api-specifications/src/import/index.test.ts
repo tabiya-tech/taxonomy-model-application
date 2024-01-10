5a7516e547031e45edaf3f1e220ddf6f3c985ada
Fides <fniragena@gmail.com>
2024-01-10 10:59:24 +0200

    chore(api-specs): import

--- api-specifications/src/import/index.test.ts
+++ api-specifications/src/import/index.test.ts
@@ -2,22 +2,24 @@ describe("Test the import module", () => {
   test("The import module can be required via the index", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
       expect(() => {
         require("./");
       }).not.toThrowError();
       // AND check if Schema is defined in it
-      expect(require("./").default.Schemas.POST.Request.Payload).toBeDefined();
+      const importModule = await import("./");
+      expect(importModule.default.Schemas.POST.Request.Payload).toBeDefined();
     }).not.toThrowError();
   });
 
   test("The import module matches the snapshot", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
-      expect(require("./").default).toMatchSnapshot();
+      const importModule = await import("./");
+      expect(importModule.default).toMatchSnapshot();
     }).not.toThrowError();
   });
 });
