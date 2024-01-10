8917fb02e290f1b91f00ce19d440b608f35cb7c2
Fides <fniragena@gmail.com>
2024-01-10 11:08:39 +0200

    chore(api-specs): modelInfo

--- api-specifications/src/modelInfo/index.test.ts
+++ api-specifications/src/modelInfo/index.test.ts
@@ -2,20 +2,19 @@ describe("Test the modelInfo module", () => {
   test("The modelInfo module can be required via the index", () => {
     //GIVEN the module
     //WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
       expect(() => {
         require("./");
       }).not.toThrowError();
-
-      let modelInfoModule = require("./").default;
+      const modelInfoModule = await import("./");
       // AND the schemas should be defined
-      expect(modelInfoModule.Schemas.GET.Response.Payload).toBeDefined();
-      expect(modelInfoModule.Schemas.POST.Response.Payload).toBeDefined();
-      expect(modelInfoModule.Schemas.POST.Request.Payload).toBeDefined();
+      expect(modelInfoModule.default.Schemas.GET.Response.Payload).toBeDefined();
+      expect(modelInfoModule.default.Schemas.POST.Response.Payload).toBeDefined();
+      expect(modelInfoModule.default.Schemas.POST.Request.Payload).toBeDefined();
 
       // AND the constants should be defined
-      const Constants = modelInfoModule.Constants;
+      const Constants = modelInfoModule.default.Constants;
       expect(Constants.NAME_MAX_LENGTH).toBeDefined();
       expect(Constants.MAX_PAYLOAD_LENGTH).toBeDefined();
       expect(Constants.DESCRIPTION_MAX_LENGTH).toBeDefined();
@@ -27,9 +26,10 @@ describe("Test the modelInfo module", () => {
   test("The export module matches the snapshot", () => {
     // GIVEN the module
     // WHEN the module is required via the index
-    expect(() => {
+    expect(async () => {
       // THEN Check if the module can be required without error
-      expect(require("./").default).toMatchSnapshot();
+      const modelInfoModule = await import("./");
+      expect(modelInfoModule.default).toMatchSnapshot();
     }).not.toThrowError();
   });
 });
