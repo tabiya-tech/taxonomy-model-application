cfca495ef33a431d0db3557fda933e745b6a978b
rav3n11 <contact.bereket.terefe@gmail.com>
2024-01-23 14:14:18 +0300

    fix(api-specs): the exportProcessState.downloadUrl schema should use a validator that ensures that the part after the method complies to the url specifications, and it is not just any string.

--- api-specifications/src/exportProcessState/schema.GET.response.ts
+++ api-specifications/src/exportProcessState/schema.GET.response.ts
@@ -4,7 +4,7 @@ import { ExportProcessState } from "./enums";
 
 const SchemaGETResponse: SchemaObject = {
   description:
-    "The state of the import process of the model. Since the import process is asynchronous, use the status to check if the import process has completed and the result to check if the process encountered any issues",
+    "The state of the export process of the model. Since the export process is asynchronous, use the status to check if the export process has completed and the result to check if the process encountered any issues",
   $id: "/components/schemas/ExportProcessStateSchema",
   type: "object",
   additionalProperties: false,
@@ -46,9 +46,19 @@ const SchemaGETResponse: SchemaObject = {
       required: ["errored", "exportErrors", "exportWarnings"],
     },
     downloadUrl: {
-      description: "The url to download the exported model.",
-      type: "string",
-      pattern: "^$|^https?://.*",
+      description:
+        "The url to download the exported model. It can be empty if the export process is still in running or it has not completed successfully.",
+      anyOf: [
+        {
+          type: "string",
+          pattern: "^$", // Allow empty string
+        },
+        {
+          type: "string",
+          format: "uri",
+          pattern: "^https://.*", // accept only https
+        },
+      ],
     },
     timestamp: {
       description: "The timestamp of the export process.",
