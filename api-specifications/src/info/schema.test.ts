bb3b5fbaa0c732cb02dad88f8f07aa1997b7b030
rav3n11 <contact.bereket.terefe@gmail.com>
2024-02-08 16:47:17 +0300

    chore(api-specs): add failure and success cases for each of the schema fields for info

--- api-specifications/src/info/schema.test.ts
+++ api-specifications/src/info/schema.test.ts
@@ -2,8 +2,15 @@ import InfoAPISpecs from "./index";
 import {
   testSchemaWithAdditionalProperties,
   testSchemaWithValidObject,
+  testStringField,
+  testTimestampField,
+  testURIField,
   testValidSchema,
 } from "_test_utilities/stdSchemaTests";
+import ModelInfoConstants from "modelInfo/constants";
+import { assertCaseForProperty, CaseType, constructSchemaError } from "_test_utilities/assertCaseForProperty";
+import { WHITESPACE } from "_test_utilities/specialCharacters";
+import InfoConstants from "info/constants";
 
 describe("Test the InfoSchema", () => {
   // GIVEN the InfoAPISpecs.Schemas.GET.Response.Payload schema
@@ -39,4 +46,90 @@ describe("Validate JSON against the Info Schema", () => {
     InfoAPISpecs.Schemas.GET.Response.Payload,
     givenValidInfoResponse
   );
+
+  describe("Validate Info.Schemas.GET.Response.Payload fields", () => {
+    describe("Test validation of 'date'", () => {
+      testTimestampField<InfoAPISpecs.Types.GET.Response.Payload>("date", InfoAPISpecs.Schemas.GET.Response.Payload);
+    });
+
+    describe("Test validation of 'path'", () => {
+      testURIField<InfoAPISpecs.Types.GET.Response.Payload>(
+        "path",
+        ModelInfoConstants.MAX_URI_LENGTH,
+        InfoAPISpecs.Schemas.GET.Response.Payload
+      );
+    });
+
+    describe("Test validation of 'branch'", () => {
+      testStringField<InfoAPISpecs.Types.GET.Response.Payload>(
+        "branch",
+        InfoConstants.BRANCH_MAX_LENGTH,
+        InfoAPISpecs.Schemas.GET.Response.Payload
+      );
+    });
+
+    describe("Test validation of 'buildNumber'", () => {
+      testStringField<InfoAPISpecs.Types.GET.Response.Payload>(
+        "buildNumber",
+        InfoConstants.BUILD_NUMBER_MAX_LENGTH,
+        InfoAPISpecs.Schemas.GET.Response.Payload
+      );
+    });
+
+    describe("Test validation of 'sha'", () => {
+      testStringField<InfoAPISpecs.Types.GET.Response.Payload>(
+        "sha",
+        InfoConstants.SHA_MAX_LENGTH,
+        InfoAPISpecs.Schemas.GET.Response.Payload
+      );
+    });
+
+    describe("Test validation of 'database'", () => {
+      test.each([
+        [
+          CaseType.Failure,
+          "undefined",
+          undefined,
+          constructSchemaError("", "required", "must have required property 'database'"),
+        ],
+        [
+          CaseType.Failure,
+          "null",
+          null,
+          [
+            constructSchemaError("/database", "type", "must be string"),
+            constructSchemaError("/database", "enum", "must be equal to one of the allowed values"),
+          ],
+        ],
+        [
+          CaseType.Failure,
+          "only whitespace characters",
+          WHITESPACE,
+          [constructSchemaError("/database", "enum", "must be equal to one of the allowed values")],
+        ],
+        [
+          CaseType.Failure,
+          "random string",
+          "foo",
+          [constructSchemaError("/database", "enum", "must be equal to one of the allowed values")],
+        ],
+        [CaseType.Success, "connected", "connected", undefined],
+        [CaseType.Success, "not connected", "not connected", undefined],
+      ])("(%s) Validate 'database' when it is %s", (caseType, _description, givenValue, failureMessages) => {
+        // GIVEN an object with the given value
+        const givenObject: InfoAPISpecs.Types.GET.Response.Payload = {
+          // @ts-ignore
+          database: givenValue,
+        };
+        // THEN expect the object to validate accordingly
+        assertCaseForProperty(
+          "database",
+          givenObject,
+          InfoAPISpecs.Schemas.GET.Response.Payload,
+          caseType,
+          failureMessages
+        );
+      });
+    });
+  });
 });
