d9f6bde569d00d099ddc8f2957ba1a0c54577caa
rav3n11 <contact.bereket.terefe@gmail.com>
2024-02-08 15:48:48 +0300

    chore(api-specs): - compose the exportProcessState/importProcessState in the modelInfo from the corresponding schema - add a max length to the downloadUrl in exportProcessState

--- api-specifications/src/importProcessState/schema.GET.response.ts
+++ api-specifications/src/importProcessState/schema.GET.response.ts
@@ -2,6 +2,40 @@ import { SchemaObject } from "ajv";
 import { RegExp_Str_ID } from "../regex";
 import { ImportProcessState } from "./enums";
 
+export const baseImportProcessStateProperties = {
+  id: {
+    description: "The identifier of the specific import state.",
+    type: "string",
+    pattern: RegExp_Str_ID,
+  },
+  status: {
+    description: "The status of the import process of the model.",
+    type: "string",
+    enum: Object.values(ImportProcessState.Enums.Status),
+  },
+  result: {
+    description:
+      "The result of the import process of the model. It can be errored, parsing errors or parsing warnings.",
+    type: "object",
+    additionalProperties: false,
+    properties: {
+      errored: {
+        description: "if the import process has completed or it was did not complete due to some unexpected error.",
+        type: "boolean",
+      },
+      parsingErrors: {
+        description: "if the import encountered errors while parsing the csv files.",
+        type: "boolean",
+      },
+      parsingWarnings: {
+        description: "if the import encountered warnings while parsing the csv files.",
+        type: "boolean",
+      },
+    },
+    required: ["errored", "parsingErrors", "parsingWarnings"],
+  },
+};
+
 const SchemaGETResponse: SchemaObject = {
   description:
     "The state of the import process of the model. Since the import process is asynchronous, use the status to check if the import process has completed and the result to check if the process encountered any issues",
@@ -9,41 +43,11 @@ const SchemaGETResponse: SchemaObject = {
   type: "object",
   additionalProperties: false,
   properties: {
-    id: {
-      description: "The identifier of the specific import state.",
-      type: "string",
-      pattern: RegExp_Str_ID,
-    },
+    ...baseImportProcessStateProperties,
     modelId: {
       description: "The identifier of the model for importing the files to.",
       type: "string",
       pattern: RegExp_Str_ID,
-    },
-    status: {
-      description: "The status of the import process of the model.",
-      type: "string",
-      enum: Object.values(ImportProcessState.Enums.Status),
-    },
-    result: {
-      description:
-        "The result of the import process of the model. It can be errored, parsing errors or parsing warnings.",
-      type: "object",
-      additionalProperties: false,
-      properties: {
-        errored: {
-          description: "if the import process has completed or it was did not complete due to some unexpected error.",
-          type: "boolean",
-        },
-        parsingErrors: {
-          description: "if the import encountered errors while parsing the csv files.",
-          type: "boolean",
-        },
-        parsingWarnings: {
-          description: "if the import encountered warnings while parsing the csv files.",
-          type: "boolean",
-        },
-      },
-      required: ["errored", "parsingErrors", "parsingWarnings"],
     },
     createdAt: { type: "string", format: "date-time" },
     updatedAt: { type: "string", format: "date-time" },
