012f5b6c1d4575214787cdcccc0c5b753d3e1bc8
rav3n11 <contact.bereket.terefe@gmail.com>
2024-02-08 16:47:44 +0300

    chore(api-specs): add failure and success cases for each of the schema fields for locale

--- api-specifications/src/locale/schema.test.ts
+++ api-specifications/src/locale/schema.test.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "crypto";
 import { getTestString } from "_test_utilities/specialCharacters";
 import {
+  testNonEmptyStringField,
   testSchemaWithAdditionalProperties,
   testSchemaWithValidObject,
+  testUUIDField,
   testValidSchema,
 } from "_test_utilities/stdSchemaTests";
 import LocaleAPISpecs from "./index";
@@ -12,25 +14,43 @@ describe("Test the Locale Schema", () => {
   // WHEN the schema is validated
   // THEN expect the schema to be valid
   testValidSchema("LocaleAPISpecs.Schemas.Payload", LocaleAPISpecs.Schemas.Payload);
-});
 
-describe("Validate JSON against the Locale Schema", () => {
-  // GIVEN a valid Locale object
-  const givenValidLocale: LocaleAPISpecs.Types.Payload = {
-    name: getTestString(LocaleAPISpecs.Constants.NAME_MAX_LENGTH),
-    UUID: randomUUID(),
-    shortCode: getTestString(LocaleAPISpecs.Constants.LOCALE_SHORTCODE_MAX_LENGTH),
-  };
+  describe("Validate JSON against the Locale Schema", () => {
+    // GIVEN a valid Locale object
+    const givenValidLocale: LocaleAPISpecs.Types.Payload = {
+      name: getTestString(LocaleAPISpecs.Constants.NAME_MAX_LENGTH),
+      UUID: randomUUID(),
+      shortCode: getTestString(LocaleAPISpecs.Constants.LOCALE_SHORTCODE_MAX_LENGTH),
+    };
 
-  // WHEN the object is validated
-  // THEN expect the object to validate successfully
-  testSchemaWithValidObject("LocaleAPISpecs.Schemas.Payload", LocaleAPISpecs.Schemas.Payload, givenValidLocale);
+    // WHEN the object is validated
+    // THEN expect the object to validate successfully
+    testSchemaWithValidObject("LocaleAPISpecs.Schemas.Payload", LocaleAPISpecs.Schemas.Payload, givenValidLocale);
 
-  // AND WHEN the object has additional properties
-  // THEN expect the object to not validate
-  testSchemaWithAdditionalProperties(
-    "LocaleAPISpecs.Schemas.Payload",
-    LocaleAPISpecs.Schemas.Payload,
-    givenValidLocale
-  );
+    // AND WHEN the object has additional properties
+    // THEN expect the object to not validate
+    testSchemaWithAdditionalProperties(
+      "LocaleAPISpecs.Schemas.Payload",
+      LocaleAPISpecs.Schemas.Payload,
+      givenValidLocale
+    );
+  });
+
+  describe("validate LocaleAPISpecs.Schemas.Payload fields", () => {
+    describe("Test validation of 'UUID'", () => {
+      testUUIDField("UUID", LocaleAPISpecs.Schemas.Payload);
+    });
+
+    describe("Test validation of 'shortCode'", () => {
+      testNonEmptyStringField(
+        "shortCode",
+        LocaleAPISpecs.Constants.LOCALE_SHORTCODE_MAX_LENGTH,
+        LocaleAPISpecs.Schemas.Payload
+      );
+    });
+
+    describe("Test validation of 'name'", () => {
+      testNonEmptyStringField("name", LocaleAPISpecs.Constants.NAME_MAX_LENGTH, LocaleAPISpecs.Schemas.Payload);
+    });
+  });
 });
