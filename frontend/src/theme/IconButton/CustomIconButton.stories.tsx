cac0b614ee7e6ca3d8a8f9fea86722258ca9ae26
Fides <fniragena@gmail.com>
2024-02-15 15:42:46 +0200

    fix(frontend): modify icon button to use default props

--- frontend/src/theme/IconButton/CustomIconButton.stories.tsx
+++ frontend/src/theme/IconButton/CustomIconButton.stories.tsx
@@ -2,6 +2,7 @@ import CustomIconButton from "./CustomIconButton";
 import { Meta, StoryObj } from "@storybook/react";
 import { ContentCopy } from "@mui/icons-material";
 import React from "react";
+import Box from "@mui/material/Box";
 
 const meta: Meta<typeof CustomIconButton> = {
   title: "Components/CustomIconButton",
@@ -15,8 +16,11 @@ type Story = StoryObj<typeof CustomIconButton>;
 
 export const Shown: Story = {
   args: {
-    icon: <ContentCopy />,
-    disabled: false,
-    ariaLabel: "copy",
+    children: (
+      <Box sx={{ padding: "5px" }}>
+        <ContentCopy />
+      </Box>
+    ),
+    title: "Copy to clipboard",
   },
 };
--- frontend/src/theme/IconButton/CustomIconButton.test.tsx
+++ frontend/src/theme/IconButton/CustomIconButton.test.tsx
@@ -1,55 +1,30 @@
+// mute the console
+import "src/_test_utilities/consoleMock";
+
 import React from "react";
 import { render, screen } from "src/_test_utilities/test-utils";
-import CustomIconButton, { DATA_TEST_ID } from "./CustomIconButton";
-import { fireEvent } from "@testing-library/react";
+import CustomIconButton from "./CustomIconButton";
 
 describe("CustomIconButton tests", () => {
+  beforeEach(() => {
+    (console.error as jest.Mock).mockClear();
+    (console.warn as jest.Mock).mockClear();
+  });
+
   test("should render with the provided icon", () => {
     // GIVEN an icon
     const icon = <svg data-testid={"test-icon"} />;
 
     // WHEN the component is rendered
-    render(<CustomIconButton handleClick={jest.fn()} icon={icon} disabled={false} ariaLabel="test label" />);
+    render(<CustomIconButton>{icon}</CustomIconButton>);
 
-    // THEN expect the icon to be in the document
-    const iconElement = screen.getByTestId("test-icon");
-    expect(iconElement).toBeInTheDocument();
-  });
-
-  test("should render the icon button with a disabled state", () => {
-    // GIVEN the component is rendered
-    // WHEN the component is rendered with the disabled prop set to true
-    render(<CustomIconButton handleClick={jest.fn()} icon={<svg />} disabled={true} ariaLabel="test label" />);
-
-    // THEN expect the button to be disabled
-    const iconButton = screen.getByTestId(DATA_TEST_ID.ICON_BUTTON);
-    expect(iconButton).toBeDisabled();
-  })
-
-  test("should call onClick when the icon button is clicked", () => {
-    // GIVEN the component is rendered
-    // Create a mock function
-    const handleClick = jest.fn();
-    render(<CustomIconButton handleClick={handleClick} icon={<svg />} disabled={false} ariaLabel="test label" />);
-
-    // WHEN the button is clicked
-    const iconButton = screen.getByTestId(DATA_TEST_ID.ICON_BUTTON);
-    fireEvent.click(iconButton);
-
-    // THEN expect onClick callback to have been called
-    expect(handleClick).toHaveBeenCalled();
-  });
-  test("should not call onClick when the icon button is clicked and disabled", () => {
-    // GIVEN the component is rendered with the disabled prop set to true
-    // Create a mock function
-    const handleClick = jest.fn();
-    render(<CustomIconButton handleClick={handleClick} icon={<svg />} disabled={true} ariaLabel="test label" />);
-
-    // WHEN the button is clicked
-    const iconButton = screen.getByTestId(DATA_TEST_ID.ICON_BUTTON);
-    fireEvent.click(iconButton);
-
-    // THEN expect onClick callback not to have been called
-    expect(handleClick).not.toHaveBeenCalled();
+    // THEN expect no errors or warning to have occurred
+    expect(console.error).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
+    // AND expect the icon to be in the document
+    const actualElement = screen.getByTestId("test-icon");
+    expect(actualElement).toBeInTheDocument();
+    // AND to match the snapshot
+    expect(actualElement).toMatchSnapshot();
   });
 });
