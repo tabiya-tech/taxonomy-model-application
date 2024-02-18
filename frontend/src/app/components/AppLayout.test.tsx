525805ca4a7695cbf2a201d6546b5705883e9cf1
Benisis, Apostolos <apostolos@benisis.de>
2024-02-18 21:41:32 +0100

    feat(frontend): Show/Hide the AppSidebar

--- frontend/src/app/components/AppLayout.test.tsx
+++ frontend/src/app/components/AppLayout.test.tsx
@@ -1,9 +1,10 @@
 // mute the console
 import "src/_test_utilities/consoleMock";
-import { render, screen } from "src/_test_utilities/test-utils";
-import AppLayout, { DATA_TEST_ID } from "./AppLayout";
+import { render, screen, waitFor } from "src/_test_utilities/test-utils";
+import AppLayout, { DATA_TEST_ID, SIDEBAR_ANIMATION_DURATION_SECONDS } from "./AppLayout";
 import { DATA_TEST_ID as APP_SIDE_BAR_ID } from "./AppSidebar";
 import { DATA_TEST_ID as APP_HEADER_ID } from "./AppHeader";
+import userEvent from "@testing-library/user-event";
 
 // Mocking the AppSidebar and AppHeader components
 jest.mock("./AppSidebar", () => {
@@ -32,7 +33,7 @@ jest.mock("./AppHeader", () => {
   };
 });
 
-describe("AppLayout Render", () => {
+describe("AppLayout render tests", () => {
   beforeEach(() => {
     (console.error as jest.Mock).mockClear();
     (console.warn as jest.Mock).mockClear();
@@ -57,6 +58,9 @@ describe("AppLayout Render", () => {
     const actualAppLayout = screen.getByTestId(DATA_TEST_ID.LAYOUT);
     expect(actualAppLayout).toBeInTheDocument();
 
+    // AND the toggle button to be present in the document
+    expect(screen.getByTestId(DATA_TEST_ID.TOGGLE_BUTTON)).toBeInTheDocument();
+
     // AND to match the snapshot
     expect(screen.getByTestId(DATA_TEST_ID.LAYOUT)).toMatchSnapshot();
 
@@ -66,5 +70,59 @@ describe("AppLayout Render", () => {
 
     // AND expect the child component to be in the document
     expect(screen.getByText("foo")).toBeInTheDocument();
+  });
+});
+
+describe("AppLayout action tests", () => {
+  beforeEach(() => {
+    (console.error as jest.Mock).mockClear();
+    (console.warn as jest.Mock).mockClear();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test("toggle button can close and reopen the add sidebar", async () => {
+    // GIVEN a Layout Children component
+    const LayoutChildren = () => <div>foo</div>;
+
+    // WHEN the AppLayout component is rendered
+    render(
+      <AppLayout>
+        <LayoutChildren />
+      </AppLayout>
+    );
+
+    // THEN expect the AppSidebar to be visible
+    await waitFor(() => {
+      expect(screen.getByTestId(APP_SIDE_BAR_ID.CONTAINER)).toBeVisible();
+    });
+    // AND  the AppHeader to be in the document
+    expect(screen.getByTestId(APP_HEADER_ID.APP_HEADER_CONTAINER)).toBeInTheDocument();
+    // AND the child component to be in the document
+    expect(screen.getByText("foo")).toBeInTheDocument();
+
+    // AND WHEN the toggle button is clicked
+    jest.useFakeTimers();
+    const toggleButton = screen.getByTestId(DATA_TEST_ID.TOGGLE_BUTTON);
+    const userEventFakeTimer = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+    await userEventFakeTimer.click(toggleButton);
+    // AND the time is advanced by the duration of the animation
+    jest.advanceTimersByTime(SIDEBAR_ANIMATION_DURATION_SECONDS * 1000);
+
+    // THEN expect the AppSidebar to be hidden
+    await waitFor(() => {
+      expect(screen.queryByTestId(APP_SIDE_BAR_ID.CONTAINER)).not.toBeVisible();
+    });
+
+    // AND WHEN the toggle button is clicked again
+    await userEventFakeTimer.click(toggleButton);
+    // AND the time is advanced by the duration of the animation
+    jest.advanceTimersByTime(SIDEBAR_ANIMATION_DURATION_SECONDS * 1000);
+    // THEN expect the AppSidebar to be shown
+    await waitFor(() => {
+      expect(screen.queryByTestId(APP_SIDE_BAR_ID.CONTAINER)).toBeVisible();
+    });
   });
 });
