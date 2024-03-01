8aaa7b0c0905fe1c77bcbe8c0503f8be4befdd4e
Fides <fniragena@gmail.com>
2024-03-01 11:21:03 +0200

    chore(frontend): assert that TextPropertyField component receives correct props for version and description model properties

--- frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesVersion/ModelPropertiesVersion.test.tsx
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesVersion/ModelPropertiesVersion.test.tsx
@@ -3,22 +3,23 @@ import "src/_test_utilities/consoleMock";
 
 import { fakeModel } from "src/modeldirectory/components/ModelsTable/_test_utilities/mockModelData";
 import { render, screen } from "src/_test_utilities/test-utils";
-import ModelPropertiesVersion, { DATA_TEST_ID, FIELD_ID } from "./ModelPropertiesVersion";
+import ModelPropertiesVersion, { DATA_TEST_ID, FIELD_ID, FIELD_LABEL_TEXT } from "./ModelPropertiesVersion";
 import ReleasedPropertyField from "src/theme/PropertyFieldLayout/ReleasedPropertyField/ReleasedPropertyField";
+import TextPropertyField from "src/theme/PropertyFieldLayout/TextPropertyField/TextPropertyField";
 
-//mock the TextPropertyField component
+// mock the TextPropertyField component
 jest.mock("src/theme/PropertyFieldLayout/TextPropertyField/TextPropertyField", () => {
-  return function ItemDetailsMock(props: { label: string; text: string; "data-testid": string; fieldId: string }) {
-    return (
+  return {
+    __esModule: true,
+    default: jest.fn().mockImplementation((props) => (
       <div data-testid={props["data-testid"]} id={props.fieldId}>
-        <span>{props.label}</span>
-        <span>{props.text}</span>
+        Text Property Field Mock
       </div>
-    );
+    )),
   };
 });
 
-// mock  the ReleasedPropertyField component
+// mock the ReleasedPropertyField component
 jest.mock("src/theme/PropertyFieldLayout/ReleasedPropertyField/ReleasedPropertyField", () => {
   return {
     __esModule: true,
@@ -47,15 +48,55 @@ describe("ModelPropertiesVersion", () => {
     // AND the UUID property to be shown
     const actualUUID = screen.getByTestId(DATA_TEST_ID.MODEL_PROPERTIES_UUID);
     expect(actualUUID).toBeInTheDocument();
+    // AND TextPropertyField component to be called with the correct props for the 'UUID'
+    expect(TextPropertyField).toHaveBeenCalledWith(
+      {
+        label: FIELD_LABEL_TEXT.LABEL_UUID,
+        text: givenModel.UUID,
+        "data-testid": DATA_TEST_ID.MODEL_PROPERTIES_UUID,
+        fieldId: FIELD_ID.UUID,
+      },
+      {}
+    );
     // AND the tabiyaPath property to be shown
     const actualTabiyaPath = screen.getByTestId(DATA_TEST_ID.MODEL_PROPERTIES_TABIYA_PATH);
     expect(actualTabiyaPath).toBeInTheDocument();
+    // AND TextPropertyField component to be called with the correct props for the 'tabiyaPath'
+    expect(TextPropertyField).toHaveBeenCalledWith(
+      {
+        label: FIELD_LABEL_TEXT.LABEL_TABIYA_PATH,
+        text: givenModel.tabiyaPath,
+        "data-testid": DATA_TEST_ID.MODEL_PROPERTIES_TABIYA_PATH,
+        fieldId: FIELD_ID.TABIYA_PATH,
+      },
+      {}
+    );
     // AND the path property to be shown
     const actualPath = screen.getByTestId(DATA_TEST_ID.MODEL_PROPERTIES_PATH);
     expect(actualPath).toBeInTheDocument();
+    // AND TextPropertyField component to be called with the correct props for the 'path'
+    expect(TextPropertyField).toHaveBeenCalledWith(
+      {
+        label: FIELD_LABEL_TEXT.LABEL_PATH,
+        text: givenModel.path,
+        "data-testid": DATA_TEST_ID.MODEL_PROPERTIES_PATH,
+        fieldId: FIELD_ID.PATH,
+      },
+      {}
+    );
     // AND the version property to be shown
     const actualVersion = screen.getByTestId(DATA_TEST_ID.MODEL_PROPERTIES_VERSION);
     expect(actualVersion).toBeInTheDocument();
+    // AND TextPropertyField component to be called with the correct props for the 'version'
+    expect(TextPropertyField).toHaveBeenCalledWith(
+      {
+        label: FIELD_LABEL_TEXT.LABEL_VERSION,
+        text: givenModel.version,
+        "data-testid": DATA_TEST_ID.MODEL_PROPERTIES_VERSION,
+        fieldId: FIELD_ID.VERSION,
+      },
+      {}
+    );
     // AND the released property to be shown
     const actualReleased = screen.getByTestId(DATA_TEST_ID.MODEL_PROPERTIES_RELEASED_STATUS);
     expect(actualReleased).toBeInTheDocument();
@@ -71,6 +112,16 @@ describe("ModelPropertiesVersion", () => {
     // AND the release notes property to be shown
     const actualReleaseNotes = screen.getByTestId(DATA_TEST_ID.MODEL_PROPERTIES_RELEASE_NOTES);
     expect(actualReleaseNotes).toBeInTheDocument();
+    // AND TextPropertyField component to be called with the correct props for the 'release notes'
+    expect(TextPropertyField).toHaveBeenCalledWith(
+      {
+        label: FIELD_LABEL_TEXT.LABEL_RELEASE_NOTES,
+        text: givenModel.releaseNotes,
+        "data-testid": DATA_TEST_ID.MODEL_PROPERTIES_RELEASE_NOTES,
+        fieldId: FIELD_ID.RELEASE_NOTES,
+      },
+      {}
+    );
     // AND to match the snapshot
     expect(modelPropertiesVersionContainer).toMatchSnapshot();
   });
