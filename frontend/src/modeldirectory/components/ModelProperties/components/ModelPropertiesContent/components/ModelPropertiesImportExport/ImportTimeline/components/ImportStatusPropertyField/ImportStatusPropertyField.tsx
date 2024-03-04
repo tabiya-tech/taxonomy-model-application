72fc17372a757adee8b04676a48e6a23ba04a789
rav3n11 <contact.bereket.terefe@gmail.com>
2024-03-04 23:51:41 +0300

    create component interfaces for importProcessState

---
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportStatusPropertyField/ImportStatusPropertyField.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {VisualMock} from "src/_test_utilities/VisualMock";
+import { ModelInfoTypes } from "src/modelInfo/modelInfoTypes";
+
+interface ImportStatusPropertyFieldProps {
+  importProcessState: ModelInfoTypes.ImportProcessState;
+  fieldId: string;
+}
+
+const uniqueId = "56ea04a0-f80e-4cd0-b967-eb55d108f1b6";
+
+export const DATA_TEST_ID = {
+  IMPORT_STATUS_FIELD: `status-${uniqueId}`,
+};
+
+/**
+ * ImportStatusPropertyField is responsible for showing the import status
+ * @param props
+ * @constructor
+ */
+
+const ImportStatusPropertyField: React.FC<ImportStatusPropertyFieldProps> = (
+  props: Readonly<ImportStatusPropertyFieldProps>
+) => {
+  return <VisualMock text="ImportStatusPropertyField" />
+};
+
+
+export default ImportStatusPropertyField;
---
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ModelPropertiesImportExport.tsx
@@ -0,0 +1,30 @@
+import {ModelInfoTypes} from "src/modelInfo/modelInfoTypes";
+import React from "react";
+import {VisualMock} from "src/_test_utilities/VisualMock";
+
+export interface ModelPropertiesImportExportProps {
+  model: ModelInfoTypes.ModelInfo;
+}
+
+const uniqueId = "7804fa94-6151-4a64-8685-cdc3d845063c";
+
+export const DATA_TEST_ID = {
+  IMPORT_TIMELINE: `import-timeline-${uniqueId}`,
+  EXPORT_TIMELINE: `export-timeline-${uniqueId}`,
+  IMPORT_EXPORT_TAB: `import-export-tab-${uniqueId}`,
+};
+
+/**
+ * ModelPropertiesImportExport is responsible for showing the import and export process state history of a model
+ * in a timeline
+ * @param props
+ * @constructor
+ */
+
+const ModelPropertiesImportExport: React.FC<ModelPropertiesImportExportProps> = (
+  props: Readonly<ModelPropertiesImportExportProps>
+) => {
+  return <VisualMock text="ModelPropertiesImportExport tab" />
+};
+
+export default ModelPropertiesImportExport;
