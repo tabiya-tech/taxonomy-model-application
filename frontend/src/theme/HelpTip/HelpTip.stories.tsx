8302b33c67d887c7e9ab9f870a2d1530c325fdca
rav3n11 <contact.bereket.terefe@gmail.com>
2024-03-05 01:14:00 +0300

    feat(frontend): HelpTip.tsx component creation

---
+++ frontend/src/theme/HelpTip/HelpTip.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from "@storybook/react";
+import HelpTip  from "./HelpTip";
+
+const meta: Meta = {
+  title: "components/HelpTip",
+  component: HelpTip,
+  tags: ["autodocs"],
+  args: {
+    children: <div>
+      <p>HelpTip is responsible for showing a tooltip that shows a helpful dialog with some react component</p>
+      <p>@param props</p>
+      <p>@constructor</p>
+    </div>
+  },
+};
+
+type Story = StoryObj<typeof HelpTip>;
+
+export const Shown: Story = {};
+
+export default meta;
