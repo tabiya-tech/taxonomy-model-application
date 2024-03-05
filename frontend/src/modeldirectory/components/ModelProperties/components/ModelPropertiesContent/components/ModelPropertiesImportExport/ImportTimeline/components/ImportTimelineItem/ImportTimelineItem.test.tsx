5e72ce6c8b3b3be6645082857319481dd3b90c8b
rav3n11 <contact.bereket.terefe@gmail.com>
2024-03-06 02:42:12 +0300

    wip: ImportTimelineItem Scenarios

---
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportTimelineItem/ImportTimelineItem.test.tsx
@@ -0,0 +1,20 @@
+describe("ImportTimelineItem", () => {
+  describe("render tests", () => {
+    test.todo("should render with provided importProcessState")
+      // GIVEN an importProcessState
+      // WHEN the component is rendered
+      // THEN expect no errors or warning to have occurred
+      // AND the component to be displayed
+      // AND the importProcessStateContent component to be displayed
+      // AND the ImportProcessStateContent to have been called with the correct props
+      // AND the timelineSeparator component to be displayed
+      // AND the timelineDot component to be displayed
+      // AND the TimelineDot to have been called with the correct props
+      // AND the timestamp to be displayed
+      // AND the TextPropertyField to have been called with the correct props
+      // AND the timelineOppositeContent component to be displayed
+      // AND the TimelineOppositeContent to have been called with the correct props
+      // AND the component to match the snapshot
+  });
+});
+
