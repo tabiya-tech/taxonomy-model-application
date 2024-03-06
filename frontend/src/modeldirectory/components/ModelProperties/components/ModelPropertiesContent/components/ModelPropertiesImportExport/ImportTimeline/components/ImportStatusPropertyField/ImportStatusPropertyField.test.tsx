9f484ec1901a3664d99c27b1e7b61af3885621f3
Merci Jacob <mercihabam@gmail.com>
2024-03-06 18:18:21 +0200

    feat(frontend): create ExportTimeLine component and its related components interfaces

--- frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportStatusPropertyField/ImportStatusPropertyField.test.tsx
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportStatusPropertyField/ImportStatusPropertyField.test.tsx
@@ -4,194 +4,92 @@ import "src/_test_utilities/consoleMock";
 import React from "react";
 import { v4 as uuid } from "uuid"
 import "@testing-library/jest-dom";
+import { ModelInfoTypes } from "src/modelInfo/modelInfoTypes";
 import { render, screen } from "src/_test_utilities/test-utils";
 import * as States from "./ImportStatusPropertyField.stories";
 import ImportProcessStateEnums from "api-specifications/importProcessState";
-import { DATA_TEST_ID as HELP_TIP_DATA_TEST_ID } from "src/theme/HelpTip/HelpTip";
 import ImportStatusPropertyField, { DATA_TEST_ID } from "./ImportStatusPropertyField";
 import { DATA_TEST_ID as ICON_DATA_TEST_ID } from "src/modeldirectory/components/ImportProcessStateIcon/ImportProcessStateIcon";
-import { ModelInfoTypes } from "../../../../../../../../../../modelInfo/modelInfoTypes";
 
 import ImportProcessState = ModelInfoTypes.ImportProcessState;
+
+
+const cases = [
+  {
+    name: "Pending",
+    state: States.Pending.args?.importProcessState,
+    icon: ICON_DATA_TEST_ID.ICON_STATUS_PENDING,
+    checkedMessages: ["Pending"]
+  },
+  {
+    name: "Running",
+    state: States.Running.args?.importProcessState,
+    icon: ICON_DATA_TEST_ID.ICON_STATUS_RUNNING,
+    checkedMessages: ["Running"]
+  },
+  {
+    name: "Completed",
+    state: States.Completed.args?.importProcessState,
+    icon: ICON_DATA_TEST_ID.ICON_STATUS_SUCCESS,
+    checkedMessages: ["Completed successfully"]
+  },
+  {
+    name: "Completed With Critical Errors",
+    state: States.CompletedWithCriticalErrors.args?.importProcessState,
+    icon: ICON_DATA_TEST_ID.ICON_STATUS_FAILED,
+    checkedMessages: ["Completed with", "critical errors"]
+  },
+  {
+    name: "Completed With Parsing Errors",
+    state: States.CompletedWithParsingErrors.args?.importProcessState,
+    icon: ICON_DATA_TEST_ID.ICON_STATUS_FAILED,
+    checkedMessages: ["Completed with", "parsing errors"]
+  },
+  {
+    name: "Completed With Parsing Warnings",
+    state: States.CompletedWithParsingWarnings.args?.importProcessState,
+    icon: ICON_DATA_TEST_ID.ICON_STATUS_FAILED,
+    checkedMessages: ["Completed with", "parsing warnings"]
+  },
+  {
+    name: "Completed With Parsing Errors and Parsing Warnings",
+    state: States.CompletedWithParsingWarningsAndParsingErrors.args?.importProcessState,
+    icon: ICON_DATA_TEST_ID.ICON_STATUS_FAILED,
+    checkedMessages: ["Completed with", "parsing errors", "parsing warnings"]
+  },
+]
 
 describe('ImportStatusPropertyField', () => {
   let id = uuid();
 
-  it("Should Render correctly for Pending State", () => {
-    // GIVEN the ImportStatusPropertyField component is rendered with pending import state.
-    const importProcessState = States.Pending.args?.importProcessState!;
+  cases.forEach((testCase) => {
+    it(`Should Render correctly for ${testCase.name}`, () => {
+      // GIVEN the ImportStatusPropertyField component is rendered with a specific import state.
+      const importProcessState: ImportProcessState = {
+        id: uuid(),
+        status: testCase.state?.status!,
+        result: testCase.state?.result!
+      };
 
-    // WHEN the component mounts
-    render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
+      // WHEN the component mounts
+      render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
 
-    const statusField = screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)
-    const message = screen.getByTestId(DATA_TEST_ID.MESSAGE)
-    const statusIcon = screen.getByTestId(ICON_DATA_TEST_ID.ICON_STATUS_PENDING)
+      // THEN it should display the correct status icon if not unkown
+      const statusIcon = screen.getByTestId(testCase.icon)
+      expect(statusIcon).toBeInTheDocument()
 
-    // THEN it status field should be rendered.
-    expect(statusField).toBeInTheDocument()
+      // AND the message should be correct
+      testCase.checkedMessages.forEach((message) => {
+        expect(screen.getAllByText(message).length).toBeGreaterThanOrEqual(1)
+      })
 
-    // THEN it should display the Pending status icon
-    expect(statusIcon).toBeInTheDocument()
+      // AND no errors or warnings should have occurred
+      expect(console.error).not.toHaveBeenCalled();
+      expect(console.warn).not.toHaveBeenCalled();
 
-    // AND the message should be pending
-    expect(message).toBeInTheDocument();
-    expect(message.innerHTML).toBe("Pending")
-
-    //AND it should not have help tip in the document
-    expect(screen.queryByTestId(HELP_TIP_DATA_TEST_ID.HELP_ICON)).not.toBeInTheDocument()
-
-    // AND no errors or warnings should have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-
-    expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
-  })
-
-  it("Should Render correctly for Running State", () => {
-    // GIVEN the ImportStatusPropertyField component is rendered with running import state.
-    const importProcessState = States.Running.args?.importProcessState!;
-
-    // WHEN the component mounts
-    render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
-
-    // THEN it should display the Running status icon
-    const statusIcon = screen.getByTestId(ICON_DATA_TEST_ID.ICON_STATUS_RUNNING)
-    expect(statusIcon).toBeInTheDocument()
-
-    // AND the message should be running
-    const message = screen.getByTestId(DATA_TEST_ID.MESSAGE)
-    expect(message).toBeInTheDocument();
-    expect(message.innerHTML).toBe("Running")
-
-    // AND no errors or warnings should have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-
-    // AND it should match the snapshot
-    expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
-  })
-
-  it("Should Render correctly for Completed Successfully", () => {
-    // GIVEN the ImportStatusPropertyField component is rendered with completed successfully import state.
-    const importProcessState = States.Completed.args?.importProcessState!;
-
-    // WHEN the component mounts
-    render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
-
-    // THEN it should display the Completed status icon
-    const statusIcon = screen.getByTestId(ICON_DATA_TEST_ID.ICON_STATUS_SUCCESS)
-    expect(statusIcon).toBeInTheDocument()
-
-    // AND the message should be completed
-    const message = screen.getByTestId(DATA_TEST_ID.MESSAGE)
-    expect(message).toBeInTheDocument();
-    expect(message.innerHTML).toBe("Completed successfully")
-
-    // AND no errors or warnings should have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-
-    // AND it should match the snapshot
-    expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
-  })
-
-  it("Should Render correctly for Completed With Critical Errors", () => {
-    // GIVEN the ImportStatusPropertyField component is rendered with completed with critical errors import state.
-    const importProcessState = States.CompletedWithCriticalErrors.args?.importProcessState!;
-
-    // WHEN the component mounts
-    render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
-
-    // THEN it should display the Completed status icon
-    const statusIcon = screen.getByTestId(ICON_DATA_TEST_ID.ICON_STATUS_FAILED)
-    expect(statusIcon).toBeInTheDocument()
-
-    // AND the message should be completed with critical errors
-    expect(screen.getByText("Completed with")).toBeInTheDocument()
-    expect(screen.getByText("critical errors")).toBeInTheDocument()
-
-    // AND no errors or warnings should have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-
-    // AND it should match the snapshot
-    expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
-  })
-
-  it("Should Render correctly for Completed With Parsing Errors", () => {
-    // GIVEN the ImportStatusPropertyField component is rendered with completed with parsing errors import state.
-    const importProcessState = States.CompletedWithParsingErrors.args?.importProcessState!;
-
-    // WHEN the component mounts
-    render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
-
-    // THEN it should display the Completed status icon
-    const statusIcon = screen.getByTestId(ICON_DATA_TEST_ID.ICON_STATUS_FAILED)
-    expect(statusIcon).toBeInTheDocument()
-
-    // AND the message should be completed with parsing errors
-    expect(screen.getByText("Completed with")).toBeInTheDocument()
-    expect(screen.getByText("parsing errors")).toBeInTheDocument()
-
-    // AND no errors or warnings should have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-
-    // It should have helper text.
-    expect(screen.getByTestId(HELP_TIP_DATA_TEST_ID.HELP_ICON)).toBeInTheDocument()
-
-    // AND it should match the snapshot
-    expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
-  })
-
-  it("Should Render correctly for Completed With Parsing Warnings", () => {
-    // GIVEN the ImportStatusPropertyField component is rendered with completed with parsing warnings import state.
-    const importProcessState = States.CompletedWithParsingWarnings.args?.importProcessState!;
-
-    // WHEN the component mounts
-    render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
-
-    // THEN it should display the Completed status icon
-    // AND the message should be completed with parsing warnings
-
-    expect(screen.getByText("Completed with")).toBeInTheDocument()
-    expect(screen.getByText("parsing warnings")).toBeInTheDocument()
-
-    // AND no errors or warnings should have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-
-
-
-    // AND it should match the snapshot
-    expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
-  })
-
-  it("Should Render correctly for Completed With Parsing Errors and Parsing Warnings", () => {
-    // GIVEN the ImportStatusPropertyField component is rendered with completed with parsing errors and parsing warnings import state.
-    const importProcessState = States.CompletedWithParsingWarningsAndParsingErrors.args?.importProcessState!;
-
-    // WHEN the component mounts
-    render(<ImportStatusPropertyField importProcessState={importProcessState} fieldId={id} />);
-
-    // THEN it should display the Completed status icon
-    const statusIcon = screen.getByTestId(ICON_DATA_TEST_ID.ICON_STATUS_FAILED)
-    expect(statusIcon).toBeInTheDocument()
-
-    // AND  It should have two helper text.
-    expect(screen.getAllByTestId(HELP_TIP_DATA_TEST_ID.HELP_ICON).length).toBe(2)
-
-    // AND the message should be completed with parsing erros and warnings.
-    expect(screen.getByText("parsing errors")).toBeInTheDocument()
-    expect(screen.getByText("parsing warnings")).toBeInTheDocument()
-
-    // AND no errors or warnings should have occurred
-    expect(console.error).not.toHaveBeenCalled();
-    expect(console.warn).not.toHaveBeenCalled();
-
-    // AND it should match the snapshot
-    expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
+      // AND it should match the snapshot
+      expect(screen.getByTestId(DATA_TEST_ID.IMPORT_STATUS_FIELD)).toMatchSnapshot()
+    })
   })
 
   it("Should return Unkown Status on Invalid case", () => {
