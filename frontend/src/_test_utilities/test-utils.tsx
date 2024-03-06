e947dce5c9fd3614e268a684872741ca121a8cf0
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2024-03-06 13:50:14 +0200

    feat(frontend): add ImportStatusPropertyField Component

--- frontend/src/_test_utilities/test-utils.tsx
+++ frontend/src/_test_utilities/test-utils.tsx
@@ -1,18 +1,17 @@
 // Based on https://testing-library.com/docs/react-testing-library/setup/
 
 import React, { ReactElement } from "react";
-import { render, RenderOptions, renderHook, RenderHookOptions } from "@testing-library/react";
+import { render, RenderOptions } from "@testing-library/react";
 import { ThemeProvider } from "@mui/material";
 import applicationTheme, { ThemeMode } from "src/theme/applicationTheme/applicationTheme";
 import SnackbarProvider from "src/theme/SnackbarProvider/SnackbarProvider";
 import { IsOnlineProvider } from "src/app/providers";
 
-export const theme = applicationTheme(ThemeMode.LIGHT);
 
-export const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <IsOnlineProvider>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={applicationTheme(ThemeMode.LIGHT)}>
         <SnackbarProvider>{children}</SnackbarProvider>
       </ThemeProvider>
     </IsOnlineProvider>
@@ -22,12 +21,6 @@ export const AllTheProviders = ({ children }: { children: React.ReactNode }) => 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
   render(ui, { wrapper: AllTheProviders, ...options });
 
-
-function customRenderHook<TProps>(ui: () => TProps, options?: Omit<RenderHookOptions<TProps>, "wrapper">){
-  return renderHook(ui, { wrapper: AllTheProviders, ...options });
-}
-
 export * from "@testing-library/react";
 
 export { customRender as render };
-export { customRenderHook as renderHook };
