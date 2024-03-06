9075021a3215f4f15d23903a4d4326f85c3e7622
Merci Jacob <mercihabam@gmail.com>
2024-03-06 18:18:21 +0200

    feat(frontend): create ExportTimeLine component and its related components interfaces

--- frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportStatusPropertyField/ImportStatusPropertyField.tsx
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportStatusPropertyField/ImportStatusPropertyField.tsx
@@ -34,81 +34,80 @@ const ImportStatusPropertyField: React.FC<ImportStatusPropertyFieldProps> = (
   const Meessage = () => {
     const { status, result } = props.importProcessState
 
-    if(status === ImportProcessStateEnums.Enums.Status.PENDING) {
-      return <Typography data-testid={DATA_TEST_ID.MESSAGE}>Pending</Typography>
-    }
+    switch (status) {
+      case ImportProcessStateEnums.Enums.Status.PENDING:
+        return <Typography data-testid={DATA_TEST_ID.MESSAGE}>Pending</Typography>
+      case ImportProcessStateEnums.Enums.Status.RUNNING:
+        return <Typography data-testid={DATA_TEST_ID.MESSAGE}>Running</Typography>
+      case ImportProcessStateEnums.Enums.Status.COMPLETED:
+        if(!result.errored && !result.parsingErrors && !result.parsingWarnings) {
+          return <Typography data-testid={DATA_TEST_ID.MESSAGE}>Completed successfully</Typography>
+        }
 
-    if(status === ImportProcessStateEnums.Enums.Status.RUNNING) {
-      return <Typography data-testid={DATA_TEST_ID.MESSAGE}>Running</Typography>
-    }
+        if(result.errored && !result.parsingErrors && !result.parsingWarnings) {
+          return (
+            <Typography data-testid={DATA_TEST_ID.MESSAGE}>
+              Completed with
+              <Box component="span" fontWeight="bold"> critical errors</Box>
+              <HelpTip>The import process could not start or complete due to a technical error. Please try again later or contact us if the problem persists.</HelpTip>
+            </Typography>
+          )
+        }
 
-    if((status === ImportProcessStateEnums.Enums.Status.COMPLETED) && !result.errored && !result.parsingErrors && !result.parsingWarnings) {
-      return <Typography data-testid={DATA_TEST_ID.MESSAGE}>Completed successfully</Typography>
-    }
+        if(!result.errored && result.parsingErrors && !result.parsingWarnings) {
+          return (
+            <Typography data-testid={DATA_TEST_ID.MESSAGE}>
+              Completed with
+              <Box component="span" fontWeight="bold"> parsing errors</Box>
+              <HelpTip>
+                The csv files have inconsistencies that could not be resolved. The model was created but is not consistent.
+                Please review the csv files and try again.
+              </HelpTip>
+            </Typography>
+          )
+        }
 
-    if((status === ImportProcessStateEnums.Enums.Status.COMPLETED) && result.errored && !result.parsingErrors && !result.parsingWarnings) {
-      return (
-          <Typography data-testid={DATA_TEST_ID.MESSAGE}>
-            Completed with
-            <Box component={"span"} fontWeight={"bold"}> critical errors</Box>
-            <HelpTip>The import process could not start or complete due to a technical error. Please try again later or contact us if the problem persists.</HelpTip>
-          </Typography>
-      )
-    }
+        if(!result.errored && !result.parsingErrors && result.parsingWarnings) {
+          return (
+            <Typography data-testid={DATA_TEST_ID.MESSAGE}>
+              Completed with
+              <Box component="span" fontWeight="bold"> parsing warnings</Box>
+              <HelpTip>
+                One or more rows or some field could not be imported, but the model was still created and is consistent.
+                Please review the csv files and try again.
+              </HelpTip>
+            </Typography>
+          )
+        }
 
-    if((status === ImportProcessStateEnums.Enums.Status.COMPLETED) && !result.errored && result.parsingErrors && !result.parsingWarnings) {
-      return (
-          <Typography data-testid={DATA_TEST_ID.MESSAGE}>
-            Completed with
-            <Box component={"span"} fontWeight={"bold"}> parsing errors</Box>
-            <HelpTip>
-              The csv files have inconsistencies that could not be resolved. The model was created but is not consistent.
-              Please review the csv files and try again.
-            </HelpTip>
-          </Typography>
-      )
-    }
+        if(!result.errored && result.parsingErrors && result.parsingWarnings) {
+          return (
+            <Typography data-testid={DATA_TEST_ID.MESSAGE}>
+              <Box component="span">Completed with </Box>
+              <Box component="span" fontWeight="bold">
+                parsing errors
+              </Box>
+              <HelpTip>
+                The csv files have inconsistencies that could not be resolved. The model was created but is not consistent.
+                Please review the csv files and try again.
+              </HelpTip>
+              and
+              <Box component="span" fontWeight="bold"> parsing warnings</Box>
+              <HelpTip>
+                One or more rows or some field could not be imported, but the model was still created and is consistent.
+                Please review the csv files and try again.
+              </HelpTip>
+            </Typography>
+          )
+        }
 
-    if((status === ImportProcessStateEnums.Enums.Status.COMPLETED) && !result.errored && !result.parsingErrors && result.parsingWarnings) {
-      return (
-        <Typography data-testid={DATA_TEST_ID.MESSAGE}>
-          Completed with
-          <Box component={"span"} fontWeight={"bold"}> parsing warnings</Box>
-          <HelpTip>
-            One or more rows or some field could not be imported, but the model was still created and is consistent.
-            Please review the csv files and try again.
-          </HelpTip>
-        </Typography>
-      )
+        return <Typography data-testid={DATA_TEST_ID.MESSAGE}>Unknown Status</Typography>
     }
-
-    if((status === ImportProcessStateEnums.Enums.Status.COMPLETED) && !result.errored && result.parsingErrors && result.parsingWarnings) {
-      return (
-        <Typography data-testid={DATA_TEST_ID.MESSAGE}>
-          <Box component={"span"}>Completed with </Box>
-          <Box component={"span"} fontWeight={"bold"}>
-            parsing errors
-          </Box>
-          <HelpTip>
-            The csv files have inconsistencies that could not be resolved. The model was created but is not consistent.
-            Please review the csv files and try again.
-          </HelpTip>
-          and
-          <Box component={"span"} fontWeight={"bold"}> parsing warnings</Box>
-          <HelpTip>
-            One or more rows or some field could not be imported, but the model was still created and is consistent.
-            Please review the csv files and try again.
-          </HelpTip>
-        </Typography>
-      )
-    }
-
-    return <Typography>Unknown Status</Typography>
   }
 
   return (
-    <PropertyFieldLayout title={"Status"} fieldId={props.fieldId}>
-      <Box display={"flex"} gap={theme.spacing(1)} alignItems={"center"} data-testid={DATA_TEST_ID.IMPORT_STATUS_FIELD}>
+    <PropertyFieldLayout title="Status" fieldId={props.fieldId}>
+      <Box display="flex" gap={theme.spacing(1)} alignItems="center" data-testid={DATA_TEST_ID.IMPORT_STATUS_FIELD}>
         <ImportProcessStateIcon test-dataId={DATA_TEST_ID.PROCCESS_STATUS_ICON} importProcessState={props.importProcessState} />
         <Meessage/>
       </Box>
