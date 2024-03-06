628142f8ce3501a5b957518024e4d03ec2bc6837
rav3n11 <contact.bereket.terefe@gmail.com>
2024-03-06 03:32:50 +0300

    wip: ImportTimelineItem tests + impl + story

--- frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportTimelineItem/ImportTimelineItem.test.tsx
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportTimelineItem/ImportTimelineItem.test.tsx
@@ -1,20 +1,71 @@
-describe("ImportTimelineItem", () => {
-  describe("render tests", () => {
-    test.todo("should render with provided importProcessState")
-      // GIVEN an importProcessState
-      // WHEN the component is rendered
-      // THEN expect no errors or warning to have occurred
-      // AND the component to be displayed
-      // AND the importProcessStateContent component to be displayed
-      // AND the ImportProcessStateContent to have been called with the correct props
-      // AND the timelineSeparator component to be displayed
-      // AND the timelineDot component to be displayed
-      // AND the TimelineDot to have been called with the correct props
-      // AND the timestamp to be displayed
-      // AND the TextPropertyField to have been called with the correct props
-      // AND the timelineOppositeContent component to be displayed
-      // AND the TimelineOppositeContent to have been called with the correct props
-      // AND the component to match the snapshot
-  });
+// mute chatty console
+import "src/_test_utilities/consoleMock";
+
+import ImportTimelineItem, {DATA_TEST_ID as IMPORT_TIMELINE_ITEM_DATA_TEST_ID } from "./ImportTimelineItem";
+import { render, screen } from "src/_test_utilities/test-utils";
+import {ModelInfoTypes} from "src/modelInfo/modelInfoTypes";
+import {
+  getOneFakeImportProcessState
+} from "src/modeldirectory/components/ModelsTable/_test_utilities/mockModelData";
+import ImportProcessStateContent, {DATA_TEST_ID as IMPROT_PROCESS_STATE_CONTENT_DATA_TEST_ID } from "src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportProcessStateContent/ImportProcessStateContent";
+import {TimelineDot, TimelineOppositeContent} from "@mui/lab";
+
+// mock the ImportProcessStateContent component
+jest.mock("src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportProcessStateContent/ImportProcessStateContent", () => {
+  const actual = jest.requireActual("src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportProcessStateContent/ImportProcessStateContent");
+  const mockImportProcessStateContent = jest.fn().mockImplementation(() => <div data-testid={actual.DATA_TEST_ID.IMPORT_PROCESS_STATE_CONTENT}>ImportProcessStateContent</div>)
+  return {
+    __esModule: true,
+    ...actual,
+    default: mockImportProcessStateContent,
+  };
 });
 
+describe("ImportTimelineItem", () => {
+  describe("render tests", () => {
+    test("should render with provided importProcessState", () => {
+      // GIVEN an importProcessState
+      const givenImportProcessState : ModelInfoTypes.ImportProcessState = getOneFakeImportProcessState(1)
+      // WHEN the component is rendered
+      // @ts-ignore
+      const timelineDotSpy = jest.spyOn(TimelineDot, "render")
+      // @ts-ignore
+      const timelineOppositeContentSpy = jest.spyOn(TimelineOppositeContent, "render")
+      render(<ImportTimelineItem importProcessState={givenImportProcessState} />);
+      // THEN expect no errors or warning to have occurred
+      expect(console.error).not.toHaveBeenCalled();
+      expect(console.warn).not.toHaveBeenCalled();
+      // AND the component to be displayed
+      const importTimelineItemComponent = screen.getByTestId(IMPORT_TIMELINE_ITEM_DATA_TEST_ID.IMPORT_TIMELINE_ITEM);
+      expect(importTimelineItemComponent).toBeInTheDocument();
+      // AND the importProcessStateContent component to be displayed
+      const importProcessStateContentComponent = screen.getByTestId(IMPROT_PROCESS_STATE_CONTENT_DATA_TEST_ID.IMPORT_PROCESS_STATE_CONTENT);
+      expect(importProcessStateContentComponent).toBeInTheDocument();
+      // AND the ImportProcessStateContent to have been called with the correct props
+      expect(ImportProcessStateContent).toHaveBeenCalledWith({importProcessState: givenImportProcessState}, {});
+      // AND the timelineSeparator component to be displayed
+      const timelineSeparatorComponent = screen.getByTestId(IMPORT_TIMELINE_ITEM_DATA_TEST_ID.TIMELINE_SEPARATOR);
+      expect(timelineSeparatorComponent).toBeInTheDocument();
+      // AND the timelineDot component to be displayed
+      const timelineDotComponent = screen.getByTestId(IMPORT_TIMELINE_ITEM_DATA_TEST_ID.TIMELINE_DOT);
+      expect(timelineDotComponent).toBeInTheDocument();
+      // AND the TimelineDot to have been called with the correct props
+      expect(timelineDotSpy).toHaveBeenCalledWith(expect.objectContaining({"data-testid": IMPORT_TIMELINE_ITEM_DATA_TEST_ID.TIMELINE_DOT}), null);
+      const timelineConnectorComponent = screen.getByTestId(IMPORT_TIMELINE_ITEM_DATA_TEST_ID.TIMELINE_CONNECTOR);
+      expect(timelineConnectorComponent).toBeInTheDocument();
+      // AND the timestamp to be displayed
+      const timestampComponent = screen.getByTestId(IMPORT_TIMELINE_ITEM_DATA_TEST_ID.IMPORT_TIMESTAMP);
+      expect(timestampComponent).toBeInTheDocument();
+      // AND the timelineOppositeContent component to be displayed
+      const timelineOppositeContentComponent = screen.getByTestId(IMPORT_TIMELINE_ITEM_DATA_TEST_ID.TIMELINE_OPPOSITE_CONTENT);
+      expect(timelineOppositeContentComponent).toBeInTheDocument();
+      // AND the TimelineOppositeContent to have been called with the correct props
+      expect(timelineOppositeContentSpy).toHaveBeenCalledWith(expect.objectContaining({
+        "data-testid": IMPORT_TIMELINE_ITEM_DATA_TEST_ID.TIMELINE_OPPOSITE_CONTENT,
+        children: expect.objectContaining({props: expect.objectContaining({children: givenImportProcessState.id})})
+      }), null);
+      // AND the component to match the snapshot
+      expect(ImportTimelineItem).toMatchSnapshot();
+    });
+  });
+});
--- frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportTimelineItem/ImportTimelineItem.tsx
+++ frontend/src/modeldirectory/components/ModelProperties/components/ModelPropertiesContent/components/ModelPropertiesImportExport/ImportTimeline/components/ImportTimelineItem/ImportTimelineItem.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-import {VisualMock} from "src/_test_utilities/VisualMock";
 import { ModelInfoTypes } from "src/modelInfo/modelInfoTypes";
+import { Typography } from "@mui/material";
+import {
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator
+} from "@mui/lab";
+import ImportProcessStateContent from "../ImportProcessStateContent/ImportProcessStateContent";
 
 interface ImportTimelineItemProps {
   importProcessState: ModelInfoTypes.ImportProcessState;
@@ -9,10 +18,12 @@ interface ImportTimelineItemProps {
 const uniqueId = "920424ec-c21e-4673-a127-6c92451a38dc";
 
 export const DATA_TEST_ID = {
+  IMPORT_TIMELINE_ITEM: `import-timeline-item-${uniqueId}`,
   IMPORT_TIMESTAMP: `import-status-${uniqueId}`,
-  IMPORT_PROCESS_STATE_CONTENT: `import-duration-${uniqueId}`,
   TIMELINE_OPPOSITE_CONTENT: `timeline-opposite-content-${uniqueId}`,
   TIMELINE_SEPARATOR: `timeline-separator-${uniqueId}`,
+  TIMELINE_CONNECTOR: `timeline-connector-${uniqueId}`,
+  TIMELINE_CONTENT: `timeline-content-${uniqueId}`,
   TIMELINE_DOT: `timeline-dot-${uniqueId}`,
 };
 
@@ -25,7 +36,22 @@ export const DATA_TEST_ID = {
 const ImportTimelineItem: React.FC<ImportTimelineItemProps> = (
   props: Readonly<ImportTimelineItemProps>
 ) => {
-  return <VisualMock text="ImportTimelineItem" />
+  return (
+      <TimelineItem data-testid={DATA_TEST_ID.IMPORT_TIMELINE_ITEM}>
+        <TimelineOppositeContent data-testid={DATA_TEST_ID.TIMELINE_OPPOSITE_CONTENT} sx={{maxWidth: "fit-content"}}>
+          <Typography variant={"body2"} data-testid={DATA_TEST_ID.IMPORT_TIMESTAMP}>
+            {props.importProcessState.id}
+          </Typography>
+          </TimelineOppositeContent>
+        <TimelineSeparator data-testid={DATA_TEST_ID.TIMELINE_SEPARATOR}>
+        <TimelineDot data-testid={DATA_TEST_ID.TIMELINE_DOT}/>
+        <TimelineConnector data-testid={DATA_TEST_ID.TIMELINE_CONNECTOR}/>
+      </TimelineSeparator>
+        <TimelineContent data-testid={DATA_TEST_ID.TIMELINE_CONTENT}>
+          <ImportProcessStateContent importProcessState={props.importProcessState} />
+        </TimelineContent>
+    </TimelineItem>
+  )
 };
 
 export default ImportTimelineItem;
