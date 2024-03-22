93a89ff19c56423c673baeb24a1fd9a4aea371d6
Fides <fniragena@gmail.com>
2024-03-22 14:19:20 +0200

    chore(api-specs): extend the maximum length of the locale short code to 20

--- api-specifications/src/locale/constants.ts
+++ api-specifications/src/locale/constants.ts
@@ -1,6 +1,6 @@
 namespace LocaleConstants {
   export const NAME_MAX_LENGTH = 256;
-  export const LOCALE_SHORTCODE_MAX_LENGTH = 3;
+  export const LOCALE_SHORTCODE_MAX_LENGTH = 20;
 }
 
 export default LocaleConstants;
