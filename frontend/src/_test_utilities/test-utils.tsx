e3e81500802b9333d3a5daa11f7cb5e0bfe2dc3a
rav3n11 <contact.bereket.terefe@gmail.com>
2024-03-24 20:56:38 +0300

    fix(frontend) - ModelHistoryTimeline component minor fixes - ModelPropertiesImport/Export component icon alignment fix - test-coverage error callback for csv parser in the modelInfo Import

--- frontend/src/_test_utilities/test-utils.tsx
+++ frontend/src/_test_utilities/test-utils.tsx
@@ -1,7 +1,7 @@
 // Based on https://testing-library.com/docs/react-testing-library/setup/
 
 import React, { ReactElement } from "react";
-import { render, RenderOptions } from "@testing-library/react";
+import { render, renderHook, RenderHookOptions, RenderOptions } from "@testing-library/react";
 import { ThemeProvider } from "@mui/material";
 import applicationTheme, { ThemeMode } from "src/theme/applicationTheme/applicationTheme";
 import SnackbarProvider from "src/theme/SnackbarProvider/SnackbarProvider";
@@ -23,5 +23,10 @@ const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) =>
   render(ui, { wrapper: AllTheProviders, ...options });
 
+function customRenderHook<T>(hook: () => T, options?: Omit<RenderHookOptions<T>, "wrapper">){
+  return renderHook(hook, { wrapper: AllTheProviders, ...options });
+}
+
 export * from "@testing-library/react";
 export { customRender as render };
+export { customRenderHook as renderHook };
