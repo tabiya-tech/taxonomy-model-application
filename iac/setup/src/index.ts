891daa933a2e49a07b7df5cdbc0db639109d8e98
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2024-03-26 19:04:01 +0200

    chore(iac): add set up environment config pipeline

--- iac/setup/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-
-
-export const environment = pulumi.getStack();
-export const tabiyaDomainName = "tabiya.tech";
-export const currentDomainName = `${environment}.${tabiyaDomainName}`;
-export const backendURl = `https://${currentDomainName}/api`;
-export const authURL = `https://${currentDomainName}/auth`;
