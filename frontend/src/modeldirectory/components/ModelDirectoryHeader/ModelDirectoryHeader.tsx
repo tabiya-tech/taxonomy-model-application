421e2a9fbf9f03e0f6653084d619d97b80110d95
Fides <fniragena@gmail.com>
2024-03-27 19:26:44 +0200

    feat(frontend): show import button based on user role

--- frontend/src/modeldirectory/components/ModelDirectoryHeader/ModelDirectoryHeader.tsx
+++ frontend/src/modeldirectory/components/ModelDirectoryHeader/ModelDirectoryHeader.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import PrimaryButton from "src/theme/PrimaryButton/PrimaryButton";
 import { AddCircleOutlined } from "@mui/icons-material";
+import { AuthContext, TabiyaUserRole } from "src/auth/AuthProvider";
 
 const uniqueId = "8482f1cc-0786-423f-821e-34b6b712d78u";
 export const DATA_TEST_ID = {
@@ -16,28 +17,34 @@ export interface ModelDirectoryHeaderProps {
 
 const ModelDirectoryHeader: React.FC<ModelDirectoryHeaderProps> = ({
   onModelImport,
-}: Readonly<ModelDirectoryHeaderProps>) => (
-  <Box
-    display="flex"
-    width="100%"
-    justifyContent="space-between"
-    flexDirection="row"
-    flexWrap="wrap"
-    alignItems="start"
-    data-testid={DATA_TEST_ID.MODEL_DIRECTORY_HEADER}
-  >
-    <Typography variant="h2" data-testid={DATA_TEST_ID.MODEL_DIRECTORY_TITLE}>
-      Model Directory
-    </Typography>
-    <PrimaryButton
-      onClick={() => onModelImport()}
-      data-testid={DATA_TEST_ID.IMPORT_MODEL_BUTTON}
-      startIcon={<AddCircleOutlined />}
-      disableWhenOffline={true}
+}: Readonly<ModelDirectoryHeaderProps>) => {
+  const { hasRole } = React.useContext(AuthContext);
+
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      justifyContent="space-between"
+      flexDirection="row"
+      flexWrap="wrap"
+      alignItems="start"
+      data-testid={DATA_TEST_ID.MODEL_DIRECTORY_HEADER}
     >
-      Import Model
-    </PrimaryButton>
-  </Box>
-);
+      <Typography variant="h2" data-testid={DATA_TEST_ID.MODEL_DIRECTORY_TITLE}>
+        Model Directory
+      </Typography>
+      {hasRole(TabiyaUserRole.ModelManager) && (
+        <PrimaryButton
+          onClick={() => onModelImport()}
+          data-testid={DATA_TEST_ID.IMPORT_MODEL_BUTTON}
+          startIcon={<AddCircleOutlined />}
+          disableWhenOffline={true}
+        >
+          Import Model
+        </PrimaryButton>
+      )}
+    </Box>
+  );
+};
 
 export default ModelDirectoryHeader;
