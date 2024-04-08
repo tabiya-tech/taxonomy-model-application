68352741dd362bac20e72af55f559a6ba77a31b6
Bereket T <berekterefe@gmail.com>
2024-04-09 00:31:28 +0300

    feat(frontend) - update api-url references to come from the env.js - adjust the pipeline to create a js file with the appropriate content

---
+++ frontend/src/envService.test.ts
@@ -0,0 +1,85 @@
+// silence chatty console
+import "src/_test_utilities/consoleMock";
+import { getEnv, getApiUrl } from "./envService";
+
+test("getEnv should return the decoded environment variable value", () => {
+  // GIVEN a key for an environment variable
+  const key = "foo";
+  // AND the environment variable is set to a base64 encoded string
+  Object.defineProperty(window, "tabiyaConfig", {
+    value: {
+      foo: btoa("bar"),
+    },
+    writable: true,
+  });
+  // WHEN getEnv is called with the key
+  const result = getEnv(key);
+  // THEN expect the decoded URL to be returned
+  expect(result).toBe("bar");
+});
+
+test("getAPI should not fail if the BACKEND_URL is not set", () => {
+  // GIVEN the BACKEND_URL environment variable is not set
+  Object.defineProperty(window, "tabiyaConfig", {
+    value: {},
+    writable: true,
+  });
+  // WHEN getApiUrl is called
+  const apiUrl = getApiUrl();
+  // THEN expect it to return an empty string
+  expect(apiUrl).toBe("");
+});
+
+test.each([
+  ["undefined", undefined],
+  ["null", null],
+])("getEnv should handle a key with a %s value gracefully", (_description: string, value) => {
+  // GIVEN a key for an environment variable
+  const key = "foo";
+  // AND the environment variable is set to an invalid base64 encoded string
+  Object.defineProperty(window, "tabiyaConfig", {
+    value: {
+      foo: value,
+    },
+    writable: true,
+  });
+  // WHEN getEnv is called with the key
+  const result = getEnv(key);
+  // THEN expect an empty string to be returned
+  expect(result).toBe("");
+});
+
+test("getApiUrl should return the API base URL", () => {
+  // GIVEN the BACKEND_URL environment variable is set to a base64 encoded string
+  const givenUrl = "https://SomeUrl.com/api";
+  Object.defineProperty(window, "tabiyaConfig", {
+    value: {
+      BACKEND_URL: btoa(givenUrl),
+    },
+    writable: true,
+  });
+  // WHEN getApiUrl is called
+  const apiUrl = getApiUrl();
+  // THEN expect it to return the decoded BACKEND_URL
+  expect(apiUrl).toBe(givenUrl);
+});
+
+test("should handle base64 decoding errors gracefully", () => {
+  // GIVEN the BACKEND_URL environment variable is set
+  Object.defineProperty(window, "tabiyaConfig", {
+    value: {
+      BACKEND_URL: "foo",
+    },
+    writable: true,
+  });
+  // AND the atob function will throw an error
+  jest.spyOn(window, "atob").mockImplementationOnce(() => {
+    throw new Error("atob error");
+  });
+  // WHEN getApiUrl is called
+  const apiUrl = getApiUrl();
+  // THEN expect it to return an empty string
+  expect(apiUrl).toBe("");
+  // AND expect an error to have been logged
+  expect(console.error).toHaveBeenCalledWith("Error loading environment variable", expect.any(Error));
+});
