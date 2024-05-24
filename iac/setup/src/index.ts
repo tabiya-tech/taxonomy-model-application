55857ca95711091d83bac85b610e9908652ef0f5
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2024-05-24 16:54:59 +0200

    chore(envs): test environment

--- iac/setup/src/index.ts
+++ iac/setup/src/index.ts
@@ -1,26 +1,49 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import { Zone } from "@pulumi/aws/route53/zone";
 
 export const environment = pulumi.getStack();
 
-export const baseDomainName = process.env.BASE_DOMAIN_NAME!;
-const domainName = environment+"."+baseDomainName;
+const PLATFORM = "platform";
 
-pulumi.log.info(`Using base domain name : ${baseDomainName}`)
-if(!domainName) throw new Error("environment variable BASE_DOMAIN_NAME is required")
+const _baseDomainName = process.env.BASE_DOMAIN_NAME!;
+
+export const baseDomainName = PLATFORM + "." + _baseDomainName;
+
+const domainName = environment + "." + PLATFORM + "." + _baseDomainName;
+
+pulumi.log.info(`Using base domain name : ${_baseDomainName}`);
+if (!_baseDomainName)
+  throw new Error("environment variable BASE_DOMAIN_NAME is required");
 
 // Create a Route 53 hosted zone for the domain
-const _hostedZone = new aws.route53.Zone("base-domain-hosted-zone", {
-  name: domainName,
-});
+let _hostedZone: Zone;
+
+let resolvedDomainName = domainName;
+
+if(environment === "production") {
+  _hostedZone = new aws.route53.Zone("base-domain-hosted-zone", {
+    name: baseDomainName,
+    tags: {
+      Environment: "production"
+    }
+  });
+  resolvedDomainName = baseDomainName;
+} else {
+  _hostedZone = new aws.route53.Zone("base-domain-hosted-zone", {
+    name: domainName,
+  });
+
+  resolvedDomainName = domainName;
+}
 
 export const hostedZone = {
   ..._hostedZone,
-  domainName
+  domainName: resolvedDomainName
 };
 
-export const targetDomainName = domainName;
-export const frontendURL = `https://${domainName}`;
-export const backendUrl = `https://${domainName}/taxonomy/api`;
-export const localesUrl = `https://${domainName}/locales/api`;
-export const authUrl = `https://auth.${domainName}`;
+export const targetDomainName = resolvedDomainName;
+export const frontendURL = `https://${resolvedDomainName}`;
+export const backendUrl = `https://${resolvedDomainName}/taxonomy/api`;
+export const localesUrl = `https://${resolvedDomainName}/locales/api`;
+export const authUrl = `https://auth.${resolvedDomainName}`;
