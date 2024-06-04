98649505b9529abefda578a36563e7f17d9761a5
Bereket T <berekterefe@gmail.com>
2024-06-04 12:46:50 +0300

    wip(general): review fixes [pulumi up]

--- pipeline/scripts/compare-versions.js
+++ pipeline/scripts/compare-versions.js
@@ -1,11 +1,11 @@
 import semver from "semver"
 import fetch from "node-fetch"
 
+// Remove the 'v' prefix if it exists
 const formatVersion = (version) => {
   if(version.startsWith('v')) {
     return version.replace("v", "");
   }
-
   return version;
 }
 
@@ -16,6 +16,7 @@ async function compareVersions(newVersion, url){
 
   let _currentVersion = formatVersion(currentVersion);
 
+  // Compare the versions, and if the old version is less than the new version, exit with 0
   if(semver.gt(_newVersion, _currentVersion)){
     console.log(`✅ New version ${newVersion}, current version ${currentVersion}`);
     process.exit(0);
@@ -25,4 +26,7 @@ async function compareVersions(newVersion, url){
   }
 }
 
+// The first argument is the new version
+// The second argument is the url to fetch the current version
+// Both are received from the GitHub Actions workflow
 compareVersions(process.argv[2], process.argv[3])
