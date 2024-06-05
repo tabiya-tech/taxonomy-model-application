fc4d40bbac48caf30f3c2d40791ec7d26ce6e0dc
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2024-06-05 20:53:21 +0200

    chore(envs): add db sanity checks

---
+++ pipeline/scripts/db-sanity-checks.js
@@ -0,0 +1,69 @@
+
+import { MongoClient } from 'mongodb';
+import semver from "semver"
+
+let job = process.argv[2];
+let currentVersion = process.argv[3];
+let dbURL = process.argv[4];
+
+const assert = (failes, message) => {
+  if(failes) {
+    console.error(`❌ failed: ${message}`)
+    process.exit(1)
+  }
+}
+
+assert(!job, "job is required")
+assert(!currentVersion, "currentVersion is required")
+assert(!dbURL, "dbURL is required")
+assert(!semver.valid(currentVersion.replace("v", "")), "currentVersion is not a valid semver version")
+
+let client = new MongoClient(dbURL);
+let _connection = client.connect();
+
+async function close(exitCode = 0){
+  if(client){
+    await client.close()
+  }
+
+  if(exitCode === 1) {
+    throw new Error("❌ failed to execute the job")
+  }
+
+  process.exit(exitCode)
+}
+
+async function handler(){
+  try {
+    let connection = await _connection;
+    let settingsCollection = await connection.db(client.db().databaseName).collection("meta");
+
+    if(job === "save") {
+      let tmpVersion = await settingsCollection.findOne({});
+
+      if(!tmpVersion){
+        await settingsCollection.insertOne({ version: currentVersion });
+      } else {
+        await settingsCollection.updateOne({}, {$set: {version: currentVersion}});
+      }
+    } else if(job === "assert") {
+      let tmpVersion = await settingsCollection.findOne({});
+
+      if(!tmpVersion){
+        console.error("❌ tmp version not found")
+        await close(1)
+      }
+
+      if(tmpVersion.version !== currentVersion){
+        console.error("❌ a new version was deployed, please initialize the tests again before deployment")
+        await close(1)
+      }
+    }
+  } catch (e) {
+    console.error(e)
+  } finally {
+    await close(0)
+  }
+}
+
+handler();
