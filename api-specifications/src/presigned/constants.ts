7b5aa9944bce6a8f1b872f9e9ae4a0e894d58b00
Anselme Irumva <61510549+irumvanselme@users.noreply.github.com>
2024-10-08 17:01:39 +0200

    fix(api-specifications): update max file size from 10MB to 20MB

--- api-specifications/src/presigned/constants.ts
+++ api-specifications/src/presigned/constants.ts
@@ -1,5 +1,5 @@
 namespace PresignedConstants {
-  export const MAX_FILE_SIZE = 1024 * 1024 * 10; // 10 MB
+  export const MAX_FILE_SIZE = 1024 * 1024 * 20; // 20 MB
   export const EXPIRES = 3600; // 1 hour
   export const MAX_URI_LENGTH = 4096;
   export const MAX_FOLDER_NAME_LENGTH = 256;
