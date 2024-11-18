39af24f7eafe7dc303d10432e2bf67bbd3348da0
Bereket Terefe <bereket.terefe@tabiya.org>
2024-11-18 12:59:13 +0300

    fix(frontend|backend) - capitalize UUIDHISTORY csv header in modelInfo.csv - RELEASED field should use lower case letters for booleans [pulumi up]

--- frontend/src/import/components/parseSelectedModelInfoFile.test.ts
+++ frontend/src/import/components/parseSelectedModelInfoFile.test.ts
@@ -1,23 +1,23 @@
 import parseSelectedModelInfoFile from "./parseSelectedModelInfoFile";
 
 describe("parseSelectedModelInfoFile tests", () => {
-  it("should parse UUIDHistory from a CSV file and return it as an array", async () => {
+  it("should parse UUIDHISTORY from a CSV file and return it as an array", async () => {
     // GIVEN a CSV file
     const testFileName = "test.csv";
-    const testFileContent = 'Name,UUIDHistory\nJohn Doe,"UUID1\nUUID2\nUUID3"'; // Sample CSV content
+    const testFileContent = 'Name,UUIDHISTORY\nJohn Doe,"UUID1\nUUID2\nUUID3"'; // Sample CSV content
 
     // AND the file is read into a File object
     const blob = new Blob([testFileContent], { type: "text/csv" });
     const file = new File([blob], testFileName, { type: "text/csv" });
 
     // WHEN parseSelectedModelInfoFile is called with the file
-    const uuidHistory = await parseSelectedModelInfoFile(file);
+    const parsedFile = await parseSelectedModelInfoFile(file);
 
     // THEN it should return the UUIDs as an array
-    expect(uuidHistory.UUIDHistory).toEqual(["UUID1", "UUID2", "UUID3"]);
+    expect(parsedFile.UUIDHistory).toEqual(["UUID1", "UUID2", "UUID3"]);
 
     // AND description should be empty
-    expect(uuidHistory.description).toEqual("");
+    expect(parsedFile.description).toEqual("");
   });
 
   it("should parse description from a CSV file and return it as a string", async () => {
@@ -25,7 +25,7 @@ describe("parseSelectedModelInfoFile tests", () => {
     const givenDescription = "This is a description";
     const givenUUIDHistory = ["UUID1", "UUID2", "UUID3"];
     const testFileName = "test.csv";
-    const testFileContent = `Name,UUIDHistory,DESCRIPTION\nJohn Doe,"${givenUUIDHistory.join(
+    const testFileContent = `Name,UUIDHISTORY,DESCRIPTION\nJohn Doe,"${givenUUIDHistory.join(
       "\n"
     )}","${givenDescription}"`; // Sample CSV content
 
@@ -55,7 +55,7 @@ describe("parseSelectedModelInfoFile tests", () => {
     await expect(parseSelectedModelInfoFile(file)).rejects.toThrow("File is not a CSV");
   });
 
-  it("should throw an error if the UUIDHistory column is not found or empty", async () => {
+  it("should throw an error if the UUIDHISTORY column is not found or empty", async () => {
     // GIVEN a CSV file with no UUIDHistory column
     const testFileName = "test.csv";
     const testFileContent = "Name\nJohn Doe";
@@ -69,7 +69,7 @@ describe("parseSelectedModelInfoFile tests", () => {
   it("should throw an error if the parser fails", async () => {
     // GIVEN a CSV file with invalid content
     const testFileName = "test.csv";
-    const testFileContent = 'Name,UUIDHistory\nJohn Doe,"UUID1\nUUID2\nUUID3"';
+    const testFileContent = 'Name,UUIDHISTORY\nJohn Doe,"UUID1\nUUID2\nUUID3"';
     // AND GIVEN the parser will fail
     jest.spyOn(require("papaparse"), "parse").mockImplementation((file, config: unknown) => {
       const mockError = { message: "Mock error during parsing" };
