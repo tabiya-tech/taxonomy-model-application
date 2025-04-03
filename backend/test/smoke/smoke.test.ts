1df8524c2392b87b94a2794821eef8457047fee8
Benisis, Apostolos <apostolos.benisis@tabiya.org>
2025-04-03 10:59:57 +0200

    fix(backend): fix smoke test timing out by adding retry logic and improve error handling for EXPECTED_VERSION_INFO parsing

--- backend/test/smoke/smoke.test.ts
+++ backend/test/smoke/smoke.test.ts
@@ -2,15 +2,31 @@ import * as supertest from "supertest";
 import { SuperAgentTest } from "supertest";
 import { StatusCodes } from "server/httpUtils";
 
-//  Output the environment variables for debugging purposes
+// Output the environment variables for debugging purposes
 console.log(`E2E_RESOURCES_BASE_URL: ${process.env.E2E_RESOURCES_BASE_URL}`);
 console.log(`E2E_BASE_URL: ${process.env.E2E_BASE_URL}`);
 console.log(`EXPECTED_VERSION_INFO: ${process.env.EXPECTED_VERSION_INFO}`);
 
-describe("Testing the deployment of the api", () => {
-  const baseUrl: string = process.env.E2E_BASE_URL as string;
-  const resourcesBaseUrl: string = process.env.E2E_RESOURCES_BASE_URL as string;
+const baseUrl: string = process.env.E2E_BASE_URL as string;
+const resourcesBaseUrl: string = process.env.E2E_RESOURCES_BASE_URL as string;
+let expectedVersion: object = {};
 
+try {
+  expectedVersion = JSON.parse(process.env.EXPECTED_VERSION_INFO as string);
+} catch (error) {
+  const err = Error("Error parsing EXPECTED_VERSION_INFO:", { cause: error });
+  console.error(err);
+  throw err;
+}
+
+/*
+ * Smoke test may fail, especially if the backend lambda function cold starts.
+ * Retry the test up to 3 times if it fails.
+ */
+
+jest.retryTimes(3, { logErrorsBeforeRetry: true });
+
+describe("Testing the deployment of the api", () => {
   beforeAll(() => {
     request = supertest.agent(baseUrl);
   });
@@ -18,12 +34,12 @@ describe("Testing the deployment of the api", () => {
   let request: SuperAgentTest;
   test("GET /info should respond with the correct version for an anonymous request", async () => {
     // GIVEN the expected version information
-    const expectedVersion = JSON.parse(process.env.EXPECTED_VERSION_INFO as string);
     expect(expectedVersion).toBeDefined();
     // AND an authorization token with the value ANONYMOUS
     const givenToken = "Bearer ANONYMOUS";
     // WHEN a GET request is made to /info
-    const response = await request.get("/info").set("Authorization", givenToken).timeout(5000);
+    //  since the jest default timeout is set to 5 secs, wait for the response for 4 secs
+    const response = await request.get("/info").set("Authorization", givenToken).timeout(4000);
 
     // THEN the response should be successful and contain the expected version information
     expect(response.statusCode).toBe(StatusCodes.OK);
