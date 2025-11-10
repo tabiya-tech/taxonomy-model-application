58bf026d0cdbf9e1f0dd5ac95227a8e02b0b5751
c5roger <c5rogers>
2025-11-10 11:39:35 +0300

    WIP(TAX-27): defining and testing extra method verbed type error schemas for POST, GET AND PATCH for TAX-27

--- api-specifications/src/error/schema.GET.test.ts
+++ api-specifications/src/error/schema.GET.test.ts
@@ -9,51 +9,44 @@ import ErrorConstants from "./constants";
 import { assertCaseForProperty, CaseType, constructSchemaError } from "_test_utilities/assertCaseForProperty";
 import { RegExp_Str_NotEmptyString } from "regex";
 import { WHITESPACE } from "_test_utilities/specialCharacters";
-import ImportAPI from "import";
-import ExportAPI from "export";
-import ModelInfoAPI from "modelInfo";
-import OccupationGroupAPI from "esco/occupationGroup";
-import OccupationAPI from "esco/occupation";
+import ModelInfoAPI from "../modelInfo";
+import OccupationGroupAPI from "../esco/occupationGroup";
+import OccupationAPI from "../esco/occupation";
 
 describe("Test the Error Schema", () => {
-  // GIVEN the APIError.Schemas.Payload schema
+  // GIVEN the APIError.Schemas.GET.Payload schema
 
   // WHEN the schema is validated
 
   // THEN expect the schema to be valid
-  testValidSchema("APIError.Schemas.Payload", APIError.Schemas.Payload);
+  testValidSchema("APIError.Schemas.GET.Payload", APIError.Schemas.GET.Payload);
 });
 
-describe("Validate JSON against the APIError Schema", () => {
-  // GIVEN a valid APIError object
-  const givenValidAPIError: APIError.Types.Payload = {
+describe("Validate JSON against the APIError GET Schema", () => {
+  // GIVEN a valid APIError GET object
+  const givenValidAPIError: APIError.Types.GET = {
+    errorCode: APIError.Constants.Common.ErrorCodes.INTERNAL_SERVER_ERROR,
     details: "Foo",
-    errorCode: APIError.Constants.ErrorCodes.INTERNAL_SERVER_ERROR,
     message: "Bar",
   };
 
   // WHEN the object is validated
   // THEN expect the object to validate successfully
-  testSchemaWithValidObject("APIError.Schemas.Payload", APIError.Schemas.Payload, givenValidAPIError);
+  testSchemaWithValidObject("APIError.Schemas.GET.Payload", APIError.Schemas.GET.Payload, givenValidAPIError);
 
   // AND WHEN the object has additional properties
   // THEN expect the object to not validate
-  testSchemaWithAdditionalProperties("APIError.Schemas.Payload", APIError.Schemas.Payload, givenValidAPIError);
+  testSchemaWithAdditionalProperties("APIError.Schemas.GET.Payload", APIError.Schemas.GET.Payload, givenValidAPIError);
 
   describe("Validate APIError.Schemas.Payload fields", () => {
     describe("Test validate of 'errorCode'", () => {
       const allValidErrorCodes = [
-        ...Object.values(ErrorConstants.ErrorCodes),
-        ...Object.values(ImportAPI.Enums.ImportResponseErrorCodes),
-        ...Object.values(ExportAPI.Enums.POST.Response.ExportResponseErrorCodes),
-        ...Object.values(ModelInfoAPI.Enums.POST.Response.ErrorCodes),
-        ...Object.values(OccupationGroupAPI.Enums.POST.Response.Status400.ErrorCodes),
-        ...Object.values(OccupationGroupAPI.Enums.POST.Response.Status404.ErrorCodes),
-        ...Object.values(OccupationGroupAPI.Enums.POST.Response.Status500.ErrorCodes),
+        ...Object.values(ErrorConstants.Common.ErrorCodes),
+        ...Object.values(ErrorConstants.GET.ErrorCodes),
+        ...Object.values(ModelInfoAPI.Enums.GET.Response.ErrorCodes),
         ...Object.values(OccupationGroupAPI.Enums.GET.Response.Status400.ErrorCodes),
         ...Object.values(OccupationGroupAPI.Enums.GET.Response.Status404.ErrorCodes),
         ...Object.values(OccupationGroupAPI.Enums.GET.Response.Status500.ErrorCodes),
-        ...Object.values(OccupationAPI.Enums.POST.Response.ErrorCodes),
         ...Object.values(OccupationAPI.Enums.GET.Response.ErrorCodes),
       ];
 
@@ -95,22 +88,22 @@ describe("Validate JSON against the APIError Schema", () => {
         //@ts-ignore
       ])("(%s) Validate errorCode when it is %s", (caseType, _description, givenValue, failureMessages) => {
         // GIVEN an object with the given value
-        const givenObject: APIError.Types.Payload = {
+        const givenObject: APIError.Types.GET = {
           //@ts-ignore
           errorCode: givenValue,
         };
         // THEN expect the object to validate accordingly
         //@ts-ignore
-        assertCaseForProperty("errorCode", givenObject, APIError.Schemas.Payload, caseType, failureMessages);
+        assertCaseForProperty("errorCode", givenObject, APIError.Schemas.GET.Payload, caseType, failureMessages);
       });
     });
 
     describe("Test validate of 'message'", () => {
-      testStringField<APIError.Types.Payload>("message", ErrorConstants.MAX_MESSAGE_LENGTH, APIError.Schemas.Payload);
+      testStringField<APIError.Types.GET>("message", ErrorConstants.MAX_MESSAGE_LENGTH, APIError.Schemas.GET.Payload);
     });
 
     describe("Test validate of 'details'", () => {
-      testStringField<APIError.Types.Payload>("details", ErrorConstants.MAX_DETAILS_LENGTH, APIError.Schemas.Payload);
+      testStringField<APIError.Types.GET>("details", ErrorConstants.MAX_DETAILS_LENGTH, APIError.Schemas.GET.Payload);
     });
   });
 });
