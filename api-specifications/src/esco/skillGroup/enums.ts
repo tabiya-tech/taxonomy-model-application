def41e0f019e035f172d6ae320f9329999486d74
c5roger <c5rogers>
2025-12-08 09:48:01 +0300

    Fix(api-specification): grouping error enums based on there http error code for skill group api-sepcification

--- api-specifications/src/esco/skillGroup/enums.ts
+++ api-specifications/src/esco/skillGroup/enums.ts
@@ -18,21 +18,45 @@ namespace SkillGroupEnums {
 
   export namespace GET {
     export namespace Response {
-      export enum ErrorCodes {
-        DB_FAILED_TO_RETRIEVE_SKILL_GROUPS = "DB_FAILED_TO_RETRIEVE_SKILL_GROUPS",
-        INVALID_LIMIT_PARAMETER = "INVALID_LIMIT_PARAMETER",
-        INVALID_NEXT_CURSOR_PARAMETER = "INVALID_NEXT_CURSOR_PARAMETER",
+      export namespace Status400 {
+        export enum ErrorCodes {
+          INVALID_LIMIT_PARAMETER = "INVALID_LIMIT_PARAMETER",
+          INVALID_NEXT_CURSOR_PARAMETER = "INVALID_NEXT_CURSOR_PARAMETER",
+        }
+      }
+      export namespace Status404 {
+        export enum ErrorCodes {
+          SKILL_GROUP_NOT_FOUND = "SKILL_GROUP_NOT_FOUND",
+          MODEL_NOT_FOUND = "GET_MODEL_NOT_FOUND",
+        }
+      }
+      export namespace Status500 {
+        export enum ErrorCodes {
+          DB_FAILED_TO_RETRIEVE_SKILL_GROUPS = "DB_FAILED_TO_RETRIEVE_SKILL_GROUPS",
+        }
       }
     }
   }
 
   export namespace POST {
     export namespace Response {
-      export enum ErrorCodes {
-        DB_FAILED_TO_CREATE_SKILL_GROUP = "DB_FAILED_TO_CREATE_SKILL_GROUP",
-        DB_FAILED_TO_RETRIEVE_A_SINGLE_SKILL_GROUP = "DB_FAILED_TO_RETRIEVE_A_SINGLE_SKILL_GROUP",
-        DB_FAILED_TO_CREATE_SKILL_GROUP_ON_ALREADY_RELEASED_MODEL = "DB_FAILED_TO_CREATE_SKILL_GROUP_ON_ALREADY_RELEASED_MODEL",
-        SKILL_GROUP_COULD_NOT_VALIDATE = "SKILL_GROUP_COULD_NOT_VALIDATE",
+      export namespace Status400 {
+        export enum ErrorCodes {
+          SKILL_GROUP_COULD_NOT_VALIDATE = "SKILL_GROUP_COULD_NOT_VALIDATE",
+          UNABLE_TO_ALTER_RELEASED_MODEL = "UNABLE_TO_ALTER_RELEASED_MODEL",
+        }
+      }
+      export namespace Status404 {
+        export enum ErrorCodes {
+          MODEL_NOT_FOUND = "POST_MODEL_NOT_FOUND",
+        }
+      }
+      export namespace Status500 {
+        export enum ErrorCodes {
+          DB_FAILED_TO_CREATE_SKILL_GROUP = "DB_FAILED_TO_CREATE_SKILL_GROUP",
+          DB_FAILED_TO_RETRIEVE_A_SINGLE_SKILL_GROUP = "DB_FAILED_TO_RETRIEVE_A_SINGLE_SKILL_GROUP",
+          DB_FAILED_TO_CREATE_SKILL_GROUP_ON_ALREADY_RELEASED_MODEL = "DB_FAILED_TO_CREATE_SKILL_GROUP_ON_ALREADY_RELEASED_MODEL",
+        }
       }
     }
   }
