5ab223b46f0b877b332d2d51ef2b2d8f04ae5b8e
c5roger <c5rogers>
2026-05-25 11:50:11 +0300

    Feat(api-specification): api-specification definetion and testing for skillGroup get api filter parameters using children ids and there associate types

--- api-specifications/src/esco/skillGroup/GET/schema.request.query.param.test.ts
+++ api-specifications/src/esco/skillGroup/GET/schema.request.query.param.test.ts
@@ -1,3 +1,5 @@
+import { getTestBase64String } from "_test_utilities/specialCharacters";
+import { assertCaseForProperty, CaseType, constructSchemaError } from "_test_utilities/assertCaseForProperty";
 import {
   testCursorField,
   testLimitField,
@@ -5,9 +7,10 @@ import {
   testSchemaWithValidObject,
   testValidSchema,
 } from "_test_utilities/stdSchemaTests";
+import { getMockId } from "_test_utilities/mockMongoId";
 
 import SkillGroupGETAPISpecs from "./index";
-import { getTestBase64String } from "_test_utilities/specialCharacters";
+import SkillGroupEnums from "../_shared/enums";
 
 describe("Test SkillGroupGETAPISpecs.Schemas.GET.Request.Query validity", () => {
   // WHEN the SkillGroupGETAPISpecs.GET.Request.Query schema
@@ -25,6 +28,12 @@ describe("Test objects against the SkillGroupGETAPISpecs.Schemas.GET.Request.Que
     cursor: getTestBase64String(SkillGroupGETAPISpecs.Constants.MAX_CURSOR_LENGTH),
   };
 
+  const givenValidSkillGroupGetQueryParameterWithChildrenFilter = {
+    ...givenValidSkillGroupGetQueryParameter,
+    childrenIds: `${getMockId(1)};${getMockId(2)}`,
+    childrenType: SkillGroupEnums.Relations.Children.ObjectTypes.SkillGroup,
+  };
+
   // WHEN the object is validated
   // THEN expect the object to validate successfully
   testSchemaWithValidObject(
@@ -33,12 +42,18 @@ describe("Test objects against the SkillGroupGETAPISpecs.Schemas.GET.Request.Que
     givenValidSkillGroupGetQueryParameter
   );
 
+  testSchemaWithValidObject(
+    "SkillGroupGETAPISpecs.Schemas.GET.Request.Query.Payload",
+    SkillGroupGETAPISpecs.Schemas.Request.Query.Payload,
+    givenValidSkillGroupGetQueryParameterWithChildrenFilter
+  );
+
   // AND WHEN the object has additional properties
   // THEN expect the object to not validate
   testSchemaWithAdditionalProperties(
     "SkillGroupGETAPISpecs.Schemas.GET.Request.Query.Payload",
     SkillGroupGETAPISpecs.Schemas.Request.Query.Payload,
-    { ...givenValidSkillGroupGetQueryParameter, extraProperty: "foo" }
+    { ...givenValidSkillGroupGetQueryParameterWithChildrenFilter, extraProperty: "foo" }
   );
 
   describe("Validate SkillGroupGETAPISpecs.Schemas.Request.Query.Payload fields", () => {
@@ -60,6 +75,92 @@ describe("Test objects against the SkillGroupGETAPISpecs.Schemas.GET.Request.Que
         false,
         false
       );
+    });
+    describe("Test validate of 'childrenIds'", () => {
+      const givenSchema = SkillGroupGETAPISpecs.Schemas.Request.Query.Payload;
+      test.each([
+        [
+          CaseType.Failure,
+          "childrenIds only",
+          { ...givenValidSkillGroupGetQueryParameter, childrenIds: getMockId(1) },
+          constructSchemaError("", "required", "must have required property 'childrenType'"),
+        ],
+        [
+          CaseType.Failure,
+          "null",
+          { ...givenValidSkillGroupGetQueryParameterWithChildrenFilter, childrenIds: null },
+          constructSchemaError("/childrenIds", "type", "must be string"),
+        ],
+        [
+          CaseType.Failure,
+          "invalid pattern",
+          { ...givenValidSkillGroupGetQueryParameterWithChildrenFilter, childrenIds: "foo" },
+          constructSchemaError("/childrenIds", "pattern", 'must match pattern "^[0-9a-f]{24}(;[0-9a-f]{24})*$"'),
+        ],
+        [
+          CaseType.Success,
+          "single id",
+          { ...givenValidSkillGroupGetQueryParameterWithChildrenFilter, childrenIds: getMockId(1) },
+          undefined,
+        ],
+        [
+          CaseType.Success,
+          "multiple ids",
+          {
+            ...givenValidSkillGroupGetQueryParameterWithChildrenFilter,
+            childrenIds: `${getMockId(1)};${getMockId(2)}`,
+          },
+          undefined,
+        ],
+      ])("%s Validate 'childrenIds' when it is %s", (caseType, _description, givenObject, failureMessage) => {
+        assertCaseForProperty("childrenIds", givenObject, givenSchema, caseType, failureMessage);
+      });
+    });
+    describe("Test validate of 'childrenType'", () => {
+      const givenSchema = SkillGroupGETAPISpecs.Schemas.Request.Query.Payload;
+      test.each([
+        [
+          CaseType.Failure,
+          "childrenType only",
+          {
+            ...givenValidSkillGroupGetQueryParameter,
+            childrenType: SkillGroupEnums.Relations.Children.ObjectTypes.Skill,
+          },
+          constructSchemaError("", "required", "must have required property 'childrenIds'"),
+        ],
+        [
+          CaseType.Failure,
+          "null",
+          { ...givenValidSkillGroupGetQueryParameterWithChildrenFilter, childrenType: null },
+          constructSchemaError("/childrenType", "type", "must be string"),
+        ],
+        [
+          CaseType.Failure,
+          "invalid enum",
+          { ...givenValidSkillGroupGetQueryParameterWithChildrenFilter, childrenType: "foo" },
+          constructSchemaError("/childrenType", "enum", "must be equal to one of the allowed values"),
+        ],
+        [
+          CaseType.Success,
+          "skill",
+          {
+            ...givenValidSkillGroupGetQueryParameterWithChildrenFilter,
+            childrenType: SkillGroupEnums.Relations.Children.ObjectTypes.Skill,
+          },
+          undefined,
+        ],
+        [
+          CaseType.Success,
+          "skillgroup",
+          {
+            ...givenValidSkillGroupGetQueryParameterWithChildrenFilter,
+            childrenType: SkillGroupEnums.Relations.Children.ObjectTypes.SkillGroup,
+          },
+          undefined,
+        ],
+      ])("%s Validate 'childrenType' when it is %s", (caseType, _description, givenObject, failureMessage) => {
+        assertCaseForProperty("childrenType", givenObject, givenSchema, caseType, failureMessage);
+      });
     });
   });
 });
