658444920d9026666afecfc5ce82add5024988f5
c5roger <c5rogers>
2026-05-25 13:54:35 +0300

    Fix: refactoring the regexp registeration

--- api-specifications/src/esco/skillGroup/_shared/regex.ts
+++ api-specifications/src/esco/skillGroup/_shared/regex.ts
@@ -1,14 +1,21 @@
 import { RegExp_Base64, RegExp_Str_Base64 } from "../../../regex";
-import { RegExp_Str_Skill_Group_Code, RegExp_Skill_Group_Code } from "../../common/regex";
+import {
+  RegExp_Str_Skill_Group_Code,
+  RegExp_Skill_Group_Code,
+  RegExp_Children_Ids,
+  RegExp_Str_Children_Ids,
+} from "../../common/regex";
 
 namespace SkillGroupRegexes {
   export namespace Str {
     export const BASE64 = RegExp_Str_Base64;
     export const SKILL_GROUP_CODE = RegExp_Str_Skill_Group_Code;
+    export const CHILDREN_IDS = RegExp_Str_Children_Ids;
   }
   export namespace RegExp {
     export const BASE64 = RegExp_Base64;
     export const SKILL_GROUP_CODE = RegExp_Skill_Group_Code;
+    export const CHILDREN_IDS = RegExp_Children_Ids;
   }
 }
 
